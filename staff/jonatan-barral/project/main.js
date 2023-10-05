dbd21ab68b5facdc3aa76e1694f547d816c7b1b9
Jonatan Barral <jinetefugaz@gmail.com>
2023-10-05 10:10:25 +0200

    add typo corrections

--- staff/jonatan-barral/project/main.js
+++ staff/jonatan-barral/project/main.js
@@ -1,35 +1,26 @@
 var loginView = document.getElementById('login-view')
 var reprisesView = document.getElementById('reprises-view')
 var eventsView = document.getElementById('events-view')
-
+var headView = document.getElementById('head-view')
+var loginButton = document.getElementById('login-button');
 
 var loginForm = loginView.querySelector('#login-form')
 loginForm.onsubmit = function(event){
-    event.preventDefault()
+event.preventDefault()  
 
-    var emailLogin = event.target['email-input-login'].value
-    var passwordLogin = event.target['password-input-login'].value
+    var nameLogin = event.target['login-secretaria'].value
+    var passwordLogin = event.target['password-login'].value
 
- console.log(emailLogin, passwordLogin) 
- 
- loginView.classList.add('off')
- homeView.classList.remove('off')
+    console.log(nameLogin, passwordLogin)
  
 }
 
-var navigateToRegister = loginView.querySelector('#register-navigate')
-navigateToRegister.onclick = function(event){
+var navigateToHead = loginView.querySelector('#head-view-navigate')
+navigateToHead.onsubmit = function(event){
     event.preventDefault()
 
     loginView.classList.add('off')
-    registerView.classList.remove('off')
-
+    headView.classList.remove('off')
+ 
 } 
 
-var navigateToLogin = registerView.querySelector('#login-navigate')
-navigateToLogin.onclick = function(event){
-    event.preventDefault()
-
-    registerView.classList.add('off')
-    loginView.classList.remove('off')
-}
