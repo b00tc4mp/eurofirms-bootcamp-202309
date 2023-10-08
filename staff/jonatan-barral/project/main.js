e587f0709d8f945de06d7a30aeabe4a14f082330
Jonatan Barral <jinetefugaz@gmail.com>
2023-10-08 02:11:08 +0200

    add changes on js code, app navigation its OK

--- staff/jonatan-barral/project/main.js
+++ staff/jonatan-barral/project/main.js
@@ -1,57 +1,98 @@
 // Obtenemos referencias a elementos HTML por su ID
-var loginView = document.getElementById('login-view'); // La vista de inicio de sesión
-loginView.style.display = '';
+var loginView = document.getElementById('login-view') // La vista de inicio de sesión
+var reprisesView = document.getElementById('reprises-view') // La vista de reprises
+var eventsView = document.getElementById('events-view') // La vista de eventos
+var headView = document.getElementById('head-view') // La vista principal
+var dressageView = document.getElementById('dressage-view')
+var paralimpicView = document.getElementById('paralimpic-view')
 
-var reprisesView = document.getElementById('reprises-view'); // La vista de reprises
-reprisesView.style.display = 'none';
+// Ocultamos todas las vistas excepto la de inicio de sesión
+loginView.style.display = ''
+reprisesView.style.display = 'none'
+eventsView.style.display = 'none'
+headView.style.display = 'none'
+dressageView.style.display = 'none'
+paralimpicView.style.display = 'none'
 
-var eventsView = document.getElementById('events-view'); // La vista de eventos
-eventsView.style.display = 'none';
-
-var headView = document.getElementById('head-view'); // La vista principal
-headView.style.display = 'none';
-
-// data
-var users = [];
-
-users[0] = { name: 'JuezC',  password: '123123123' };
-users[1] = { name: 'secretaria', password: '123123123' };
+// Datos de usuarios
+var users = [
+    { name: 'JuezC', password: '123123123' },
+    { name: 'secretaria', password: '123123123' }
+]
 
 // Obtenemos el formulario de inicio de sesión dentro de la vista de inicio de sesión
-var loginForm = loginView.querySelector('#login-form');
+var loginForm = loginView.querySelector('#login-form')
 loginForm.onsubmit = function (event) {
-    event.preventDefault();
+    event.preventDefault()
 
-    var usernameInput = loginForm.querySelector('#user');
-    var passwordInput = loginForm.querySelector('#password-login');
+    var usernameInput = loginForm.querySelector('#user')
+    var passwordInput = loginForm.querySelector('#password-login')
 
-    var username = usernameInput.value;
-    var password = passwordInput.value;
+    var username = usernameInput.value
+    var password = passwordInput.value
 
-    var foundUser = null;
+    var foundUser = null
 
     for (var i = 0; i < users.length; i++) {
-        var user = users[i];
+        var user = users[i]
 
-        if (user.name === username) {  // Cambia "email" a "name"
-            foundUser = user;
-            break;
+        if (user.name === username) {
+            foundUser = user
+            break
         }
     }
 
-    if (foundUser === null) {
-        alert('Credenciales incorrectas');
-        return;
+    if (foundUser === null || foundUser.password !== password) {
+        alert('Credenciales incorrectas')
+        return
     }
 
-    if (foundUser.password !== password) {
-        alert('Credenciales incorrectas');
-        return;
-    }
+    usernameInput.value = ''
+    passwordInput.value = ''
 
-    usernameInput.value = '';
-    passwordInput.value = '';
+    loginView.style.display = 'none'
+    headView.style.display = ''
+}
 
-    loginView.style.display = 'none';
-    headView.style.display = '';
+// Gestión de enlaces y botones para cambiar de vistas
+var reprisesLink = headView.querySelector('#reprises-link')
+reprisesLink.onclick = function (event) {
+    event.preventDefault()
+    headView.style.display = 'none'
+    reprisesView.style.display = ''
+}
+
+var dressageLink = reprisesView.querySelector('#dressage-link')
+dressageLink.onclick = function (event) {
+    event.preventDefault()
+    reprisesView.style.display = 'none'
+    dressageView.style.display = ''
+}
+
+var goBackDressage = dressageView.querySelector('#go-back')
+goBackDressage.onclick = function (event) {
+    event.preventDefault()
+    dressageView.style.display = 'none'
+    reprisesView.style.display = ''
+}
+
+var paralimpicLink = reprisesView.querySelector('#paralimpic-link')
+paralimpicLink.onclick = function (event) {
+    event.preventDefault()
+    reprisesView.style.display = 'none'
+    paralimpicView.style.display = ''
+}
+
+var goBackParalimpic = paralimpicView.querySelector('#go-back')
+goBackParalimpic.onclick = function (event) {
+    event.preventDefault()
+    paralimpicView.style.display = 'none'
+    reprisesView.style.display = ''
+}
+var eventsLink = document.getElementById('events-link')
+eventsLink.onclick = function(event) {
+    event.preventDefault()
+
+    headView.style.display = 'none'
+    eventsLink.style.display = ''
 }
