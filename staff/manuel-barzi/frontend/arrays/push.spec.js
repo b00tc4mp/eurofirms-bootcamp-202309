6563afdc1c6df059169e0489c906f837b492f4fb
Manuel Barzi <manuelbarzi@gmail.com>
2023-10-13 13:09:15 +0200

    add arrays standalone functions base code

---
+++ staff/manuel-barzi/frontend/arrays/push.spec.js
@@ -0,0 +1,12 @@
+console.log('%cTEST push', 'color: magenta; font-weight: bold;')
+
+console.log('CASE push horse in array [pig, goat, sheep]')
+
+var farm = ['pig', 'goat', 'sheep']
+
+var length = push(farm, 'horse')
+
+console.log(length)
+// 4
+console.log(farm)
+// [pig, goat, sheep, horse]
