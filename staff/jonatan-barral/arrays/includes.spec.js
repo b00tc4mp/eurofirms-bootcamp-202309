96aec1b33a918da801ed42562292044c386d7a5d
Jonatan Barral <jinetefugaz@gmail.com>
2023-10-20 10:20:24 +0200

    add chanjes

---
+++ staff/jonatan-barral/arrays/includes.spec.js
@@ -0,0 +1,35 @@
+console.log('CASE check array [10, 20, 30, 40, 50] includes 40')
+
+var nums = [10, 20, 30, 40, 50]
+
+var exists = includes(nums, 40)
+
+console.log(exists)
+// true
+
+console.log('CASE check array [10, 20, 30, 40, 50] includes 60')
+
+var nums = [10, 20, 30, 40, 50]
+
+var exists = includes(nums, 60)
+
+console.log(exists)
+// false
+
+console.log('CASE check array [cat, dog, lion, tiger, monkey, snake, horse] includes tiger')
+
+var animals = ['cat', 'dog', 'lion', 'tiger', 'monkey', 'snake', 'horse']
+
+var exists = includes(animals, 'tiger')
+
+console.log(exists)
+// true
+
+console.log('CASE check array [cat, dog, lion, tiger, monkey, snake, horse] includes elephant')
+
+var animals = ['cat', 'dog', 'lion', 'tiger', 'monkey', 'snake', 'horse']
+
+var exists = includes(animals, 'elephant')
+
+console.log(exists)
+// false
