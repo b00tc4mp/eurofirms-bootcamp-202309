eea64f075712878b5d8e230def7393f1894f14c5
David Cano <webdelcano@gmail.com>
2023-10-23 22:39:01 +0200

    add new styles to the app

--- staff/david-cano/frontend/app/data.js
+++ staff/david-cano/frontend/app/data.js
@@ -1,45 +1,51 @@
+// DATA BASE, creamos una base de datos (ficticia) con un array vacío para usuarios y posts
 var users = []
 
-users[0] = {
+// ingresamos 2 usuarios de ejemplo
+users[0] = { 
     name: 'Pepito Grillo',
     email: 'pepito@grillo.com',
-    password: '123123123'
-}
+    password: '123123123' }
 
-users[1] = {
+users[1] = { 
     name: 'Campa Nilla',
     email: 'campa@nilla.com',
-    password: '123123123'
-}
+    password: '123123123' }
 
+ // Agregamos algunos posts de ejemplo   
 var posts = []
 
 posts[0] = {
     author: 'pepito@grillo.com',
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png',
+    alt: 'Icono carita sonriente',
     text: 'Smile!'
 }
 
 posts[1] = {
     author: 'campa@nilla.com',
     image: 'https://www.telemundo.com/sites/nbcutelemundo/files/styles/fit-1240w/public/sites/nbcutelemundo/files/images/article/2014/08/28/hello_kitty_140920568644_4.jpg',
+    alt: 'Icono carita sonriente',
     text: 'Hello, Kitty!'
 }
 
 posts[2] = {
     author: 'campa@nilla.com',
     image: 'https://m.media-amazon.com/images/I/51-qisfjMnL.jpg',
+    alt: 'Icono carita sonriente',
     text: 'Doraemon'
 }
 
 posts[3] = {
     author: 'pepito@grillo.com',
     image: 'https://i.ebayimg.com/images/g/V9wAAOSw~e5ZU~Ls/s-l1200.webp',
-    text: 'Pikachu!'
+    alt: 'Icono carita sonriente',
+    text: 'Pikatchu!'
 }
 
 posts[4] = {
     author: 'pepito@grillo.com',
     image: 'https://i.pinimg.com/550x/64/65/90/6465907c690be529106e4ada2c94d0d6.jpg',
+    alt: 'Icono carita sonriente',
     text: 'La Hormiga Atómica!'
 }
--- staff/david-cano/frontend/app/views/register.js
+++ staff/david-cano/frontend/app/views/register.js
@@ -1,68 +1,96 @@
-// register view
+// REGISTER VIEW, div que contiene la página con el formulario de registro
 
+// declaramos una variable, "registerView", para el div que contiene el formulario de registro con el id register-view en el html
 var registerView = document.getElementById('register-view')
 
+// le damos un estilo display none, a la variable "registerView", para que no aparezca la pagina de registro
 registerView.style.display = 'none'
 
-// navigation to login
+// NAVIGATION TO LOGIN, enlace a la pagina de login <a>
 
+// declaramos una variable para el enlace a login con el id login-link en el html
 var loginLink = registerView.querySelector('#login-link')
 
+// le aplicamos el metodo onclick al enlace a login en el formulario de registro
 loginLink.onclick = function (event) {
+
+// le decimos que deje de cargar la página por defecto
     event.preventDefault()
 
+// hacemos que desaparezca el formulario de registro y que aparezca el de login
     registerView.style.display = 'none'
     loginView.style.display = ''
 }
 
-// submit for register
+// SUBMIT FOR REGISTER, entregar el registro, enviar el registro
 
+//declaramos una variable, "registerForm", para el formulario de registro con id register-form en el html
 var registerForm = registerView.querySelector('#register-form')
 
+// le aplicamos el método onsubmit (ya que es un formulario)
 registerForm.onsubmit = function (event) {
+
+// le decimos que deje de cargar la página por defecto
     event.preventDefault()
 
-    var nameInput = registerForm.querySelector('#name-input')
-    var emailInput = registerForm.querySelector('#email-input')
-    var passwordInput = registerForm.querySelector('#password-input')
+// declaramos 3 variables para los campos de los inputs nombre, email y contraseña que tengan los id name-register, email-register, password-register en el html
+    var nameInput = registerForm.querySelector('#name-register')
+    var emailInput = registerForm.querySelector('#email-register')
+    var passwordInput = registerForm.querySelector('#password-register')
 
+// declaramos 3 variables para obtener el valor de los inputs nombre, email y contraseña, ahora en las variables nameInput, emailInput y passwordInput, introducidos por el usuario
     var name = nameInput.value
     var email = emailInput.value
     var password = passwordInput.value
 
-    // search user by email
+    //BUSCAR USUARIO POR EMAIL PARA COMPROBAR SI YA ESTÁ REGISTRADO
 
-    var foundUser = null
+// declaramos una variable usuarioEncontrado, "foundUser", y le asignamos un valor nulo, vacío
+var foundUser = null
 
-    for (var i = 0; i < users.length; i++) {
-        var user = users[i]
+// creamos un bucle for para recorrer el array usuarios
+for (var i = 0; i < users.length; i++) {
 
-        if (user.email === email) {
-            foundUser = user
+    // declaramos una variable usuario para que vaya recogiendo las iteraciones del for en el array usuarios al recorrerlo
+    var user = users[i]
 
-            break
-        }
+    // condición if, si el valor del email de un usuario, dentro del array usuarios, es estrictamente igual al valor del campo email, del login, introducido por el usuario, usuario encontrado 
+    if (user.email === email) {
+        foundUser = user
+
+        //para, ya está
+        break
     }
+}
 
-    // if user exists (it was found) then error
+//SI EL USUARIO EXISTE, SI FUE ENCONTRADO, ALERTA DE ERROR
 
-    if (foundUser !== null) {
-        alert('User already exists')
+  // si la variable usuario encontrado no es nula, alerta: el usuario ya existe
+  if (foundUser !== null) {
+    alert('User already exists')
+// y salimos de la función con return
+    return
+}
 
-        return
-    }
+//PARA GUARDAR LOS NUEVOS DATOS DEL USUARIO REGISTRADO
 
+// ahora declaramos una variable usuario y le asignamos un valor de OBJETO vacío
     var user = {}
+
+// le agregamos al objeto vacio usuario, "user", las propiedades nombre, email y contraseña y les asignamos los valores de los inputs nombre, email y contraseña introducidos por el usuario en el formulario de registro
     user.name = name
     user.email = email
     user.password = password
 
+// le decimos que guarde esos valores agregados del nombre, email y contraseña, que ahora están en la variable usuario, "user", en el array usuarios, "users"
     users.push(user)
 
+//limpiamos los valores de los campos del formulario de registro
     nameInput.value = ''
     emailInput.value = ''
     passwordInput.value = ''
 
+// hacemos que desaparezca el formulario de registro y aparezca el de login
     registerView.style.display = 'none'
     loginView.style.display = ''
 }
