39f01264af1b17879fd1cc050c51599f0a97537e
Eileen Lopez <eileenicolelopzi@gmail.com>
2023-10-24 17:29:41 +0200

    add arroz tests

---
+++ staff/eileen-lopez/frontend/arroz/Arroz.prototype.pop.spec.js
@@ -0,0 +1,19 @@
+TEST('Arroz prototype pop')
+
+CASE('pop tomato from arroz plants')
+
+const plants = new Arroz
+plants[0] = 'broccoli'
+plants[1] = 'cauliflower'
+plants[2] = 'cabbage'
+plants[3] = 'kale'
+plants[4] = 'tomato'
+plants.length = 5
+
+var extracted = plants.pop()
+
+crossOriginIsolated.log(extracted)
+// tomato
+
+console.log(plants)
+// Arroz { 0: broccoli, 1: cauliflower, 2: cabbage, 3: kale, length: 4 }
