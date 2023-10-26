74580d580cdaaaf2cbfc30400905f7878e0223a7
Jonatan Barral <jinetefugaz@gmail.com>
2023-10-26 15:37:35 +0200

    add logics of create a new post and render posts

--- staff/jonatan-barral/frontend/app/views/home.js
+++ staff/jonatan-barral/frontend/app/views/home.js
@@ -1,4 +1,3 @@
-// home view
 
 homeView = document.getElementById('home-view')
 
@@ -15,17 +14,17 @@ logoutButton.onclick = function () {
     loginView.style.display = ''
 }
 
-// post panel
+
 
 newPostPanel = homeView.querySelector('#new-post-panel')
 
 newPostPanel.style.display = 'none'
 
-// post form
+
 
 newPostForm = newPostPanel.querySelector('#new-post-form')
 
-// post button
+
 
 newPostButton = homeView.querySelector('#new-post-button')
 
@@ -33,11 +32,11 @@ newPostButton.onclick = function () {
     newPostPanel.style.display = ''
 }
 
-// cancel post button
 
-cancelPostButton = newPostForm.querySelector('#cancel-post-button')
 
-cancelPostButton.onclick = function (event) {
+cancelNewPostButton = newPostForm.querySelector('#cancel-new-post-button')
+
+cancelNewPostButton.onclick = function (event) {
     event.preventDefault()
 
     newPostForm.reset()
@@ -45,7 +44,7 @@ cancelPostButton.onclick = function (event) {
     newPostPanel.style.display = 'none'
 }
 
-// submit post form
+
 
 newPostForm.onsubmit = function (event) {
     event.preventDefault()
@@ -58,51 +57,53 @@ newPostForm.onsubmit = function (event) {
     var imageDescription = imageDescriptionInput.value
     var text = textInput.value
 
-    var post = {}
-    post.author = loggedInEmail
+    try {
+        createNewPost(loggedInEmail, image, imageDescription, text)
 
-    post.image = image
-    post.imageDescription = imageDescription
-    post.text = text
+        newPostForm.reset()
 
-    posts.push(post)
+        newPostPanel.style.display = 'none'
 
-    newPostForm.reset()
-
-    newPostPanel.style.display = 'none'
-
-    renderPosts()
+        renderPosts()
+    } catch (error) {
+        alert(error.message)
+    }
 }
 
-// render posts
+
 
 function renderPosts() {
-    var postsList = homeView.querySelector('#posts-list')
+    try {
+        var posts = retrievePosts(loggedInEmail)
 
-    postsList.innerHTML = ''
+        var postsList = homeView.querySelector('#posts-list')
 
-    for (var i = posts.length - 1; i >= 0; i--) {
-        var post = posts[i]
+        postsList.innerHTML = ''
 
-        var article = document.createElement('article')
-        article.setAttribute('aria-label', 'Post')
+        for (var i = posts.length - 1; i >= 0; i--) {
+            var post = posts[i]
 
-        var span = document.createElement('span')
-        span.innerText = post.author
-        span.setAttribute('aria-label', 'author')
-        var image = document.createElement('img')
-        image.setAttribute('class', 'post-image')
-        image.src = post.image
-        image.alt = post.imageDescription
+            var article = document.createElement('article')
+            article.setAttribute('aria-label', 'Post')
+            var h3 = document.createElement('h3')
+            h3.innerText = post.author //The aria-level label is removed due to compatibility issues with the screen reader and the h3 tag.
 
 
-        var paragraph = document.createElement('p')
-        paragraph.innerText = post.text
+            var image = document.createElement('img')
+            image.setAttribute('class', 'post-image')
+            image.src = post.image
+            image.alt = post.imageDescription
 
-        article.appendChild(span)
-        article.appendChild(image)
-        article.appendChild(paragraph)
+            var paragraph = document.createElement('p')
+            paragraph.innerText = post.text
 
-        postsList.appendChild(article)
+            article.appendChild(h3)
+            article.appendChild(image)
+            article.appendChild(paragraph)
+
+            postsList.appendChild(article)
+        }
+    } catch (error) {
+        alert(error.message)
     }
 }
