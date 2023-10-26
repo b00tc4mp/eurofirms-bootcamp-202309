962857e505b845e873a0cfa527dacc9c640c402c
Jonatan Barral <jinetefugaz@gmail.com>
2023-10-26 12:16:08 +0200

    remuve duplicate arrays folder

--- staff/jonatan-barral/arrays/foreach.js
+++
@@ -1,8 +0,0 @@
-function forEach(array, callback) {
-    for (var i = 0; i < array.length; i++) {
-            var element = array[i]
-
-        callback(element)
-    }
-}
- 
--- staff/jonatan-barral/arrays/some.js
+++
@@ -1,8 +0,0 @@
-function some(array, callback) {
-    for (var i = 0; i < array.length; i++) {
-        var element = array[i]
-        var result = callback(element)
-        if (result) return true
-    }
-    return false
-}   
