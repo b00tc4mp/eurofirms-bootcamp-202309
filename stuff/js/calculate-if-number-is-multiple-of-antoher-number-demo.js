d4e16421cad3e09ed13a54cf3a392bc176007aab
Manuel Barzi <manuelbarzi@gmail.com>
2023-10-27 12:37:54 +0200

    add check number is multiple of another demo

---
+++ stuff/js/calculate-if-number-is-multiple-of-antoher-number-demo.js
@@ -0,0 +1,27 @@
+// DEMO is num1 multiple of num2?
+
+// CASE 1
+
+var num1 = 10
+var num2 = 11
+
+var v1 = num1 / num2
+var v2 = Math.round(v1)
+
+var eq = v1 === v2
+
+console.log(eq)
+// false
+
+// CASE 2
+
+var num1 = 22
+var num2 = 11
+
+var v1 = num1 / num2
+var v2 = Math.round(v1)
+
+var eq = v1 === v2
+
+console.log(eq)
+// true
