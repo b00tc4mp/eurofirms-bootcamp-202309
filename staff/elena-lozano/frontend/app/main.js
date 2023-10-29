f89122bb47d1a93b3b8aa0c7f0d977fced279f94
Elena Lozano <lozano.llopis@gmail.com>
2023-10-29 18:38:17 +0100

    add new files in my app

--- staff/elena-lozano/frontend/app/main.js
+++ staff/elena-lozano/frontend/app/main.js
@@ -1,128 +1,26 @@
-// data (aqui se almacenan unos datos que nos hemos inventado, como si se hubiesen regustrado)
+// global variables
 
-var users = []
+// session
 
-users[0] = { name: 'Pepito Grillo', email: 'pepito@grillo.com', password: '123123123' }
+var loggedInEmail = null
 
-users[1] = { name: 'Campa Nilla', email: 'campa@nilla.com', password: '123123123' }
+// register
 
-// register view
+var registerView
+var loginLink
+var registerForm
 
-var registerView = document.getElementById('register-view')
+// login
 
-registerView.style.display = 'none'
+var loginView
+var registerLink
+var loginForm
 
-// navigation to login
+// home
 
-var loginLink = registerView.querySelector('#login-link')
-
-loginLink.onclick = function (event) {
-    event.preventDefault()
-
-    registerView.style.display = 'none'
-    loginView.style.display = ''
-}
-
-// submit for register
-
-var registerForm = registerView.querySelector('#register-form')
-
-registerForm.onsubmit = function (event) {
-    event.preventDefault()
-
-    var nameInput = registerForm.querySelector('#name-input')
-    var emailInput = registerForm.querySelector('#email-input')
-    var passwordInput = registerForm.querySelector('#password-input')
-
-    var name = nameInput.value
-    var email = emailInput.value
-    var password = passwordInput.value
-
-    var user = {}
-    user.name = name
-    user.email = email
-    user.password = password
-
-    users.push(user)
-
-    nameInput.value = ''
-    emailInput.value = ''
-    passwordInput.value = ''
-
-    registerView.style.display = 'none'
-    loginView.style.display = ''
-}
-
-// login view
-
-var loginView = document.getElementById('login-view')
-
-loginView.style.display = ''
-
-// navigation to register
-
-var registerLink = loginView.querySelector('#register-link')
-
-registerLink.onclick = function (event) {
-    event.preventDefault()
-
-    loginView.style.display = 'none'
-    registerView.style.display = ''
-}
-
-// submit login
-
-var loginForm = loginView.querySelector('#login-form')
-
-loginForm.onsubmit = function (event) {
-    event.preventDefault()
-
-    var emailInput = loginForm.querySelector('#email-input')
-    var passwordInput = loginForm.querySelector('#password-input')
-
-    var email = emailInput.value
-    var password = passwordInput.value
-
-    var foundUser = null
-
-    for (var i = 0; i < users.length; i++) {
-        var user = users[i]
-
-        if (user.email === email) {
-            foundUser = user
-
-            break
-        }
-    }
-
-    if (foundUser === null) {
-        alert('Wrong credentials')
-
-        return
-    }
-
-    if (foundUser.password !== password) {
-        alert('Wrong credentials')
-
-        return
-    }
-
-    emailInput.value = ''
-    passwordInput.value = ''
-
-    loginView.style.display = 'none'
-    homeView.style.display = ''
-}
-
-// home view
-
-var homeView = document.getElementById('home-view')
-
-homeView.style.display = 'none'
-
-var logoutButton = homeView.querySelector('#logout-button')
-
-logoutButton.onclick = function () {
-    homeView.style.display = 'none'
-    loginView.style.display = ''
-}
+var homeView
+var logoutButton
+var newPostPanel
+var newPostForm
+var newPostButton
+var cancelNewPostButton
