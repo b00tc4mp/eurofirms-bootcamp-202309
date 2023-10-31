385f7e3ffb4a54725e1ce9f0397a0d1d03a86184
Eileen Lopez <eileenicolelopzi@gmail.com>
2023-10-31 18:17:43 +0100

    make app use react for view/register

---
+++ staff/eileen-lopez/frontend/app/components/LogoTemp.jsx
@@ -0,0 +1,33 @@
+function Logo() {
+    return  <svg width="154.03mm" height="149.04mm" version="1.1" viewBox="0 0 154.03 149.04">
+    <g transform="translate(78.126 67.393)">
+    <g transform="translate(-39.001 -148.55)">
+    <g fill="#fff">
+    <rect x="-39.125" y="155.68" width="72.559" height="74.517"/>
+    <rect x="-39.125" y="81.158" width="73.498" height="74.517"/>
+    <rect x="37.381" y="155.67" width="77.523" height="74.517"/>
+    <rect x="42.345" y="81.157" width="72.559" height="74.517"/>
+    </g>
+    <g fill="#ffe680">
+    <path d="m-39.125 155.68 72.559 74.517 3.9467-7.5e-4v-74.516z"/>
+    <path d="m37.381 155.68v74.516l74.516-74.516z"/>
+    <path d="m37.381 81.155v74.52h74.516l-69.552-74.518z"/>
+    <path d="m37.381 81.155-74.52 74.52h74.52z"/>
+    </g>
+    <g strokeLinecap="round" strokeLinejoin="round">
+    <path d="m33.434 155.67h8.9108v23.991l36.915 7.7e-4 23.991-23.991h11.653l-74.518 74.518h-6.9524zm8.9108 32.51v28.397l28.397-28.397z" strokeLinecap="round" strokeLinejoin="round" StrokeWidth="6.9393" style="white-space:pre"/>
+    <path d="m42.345 155.67h-8.9108v-23.991l-36.915-8.2e-4 -23.991 23.991h-11.653l73.499-74.518h7.9711zm-8.9108-32.51v-28.397l-28.397 28.397z" strokeLinecap="round" strokeLinejoin="round" StrokeWidth="6.9393" style="white-space:pre"/>
+    <g transform="matrix(6.9393 0 0 7.0411 -79.35 -54.301)">
+    <path d="m20.148 29.822h-1.1571v-10.583h1.1571z" style={{'white-space':'pre'}}/>
+    <path d="m27.993 29.822h-1.6792l-3.6124-3.7536v3.7536h-1.1712v-10.583h1.1712v5.2917l3.3161-2.3848 1.7357-0.01411-4.2333 3.1609z" style={{'white-space':'pre'}} />
+    </g>
+    <g transform="matrix(-6.9393 0 0 -7.041 338.07 288.91)" strokeLinecap="round" strokeLinejoin="round">
+    <path d="m46.652 18.923h-1.1571v-10.583h1.1571z" style={{'white-space':'pre'}}/>
+    <path d="m48.896 8.3399q0.381 0 0.635 0.26811 0.26811 0.254 0.26811 0.635 0 0.36689-0.26811 0.635-0.254 0.254-0.635 0.254-0.36689 0-0.635-0.26811-0.254-0.26811-0.254-0.62089 0-0.36689 0.254-0.635 0.26811-0.26811 0.635-0.26811zm0.59267 10.583h-1.1712v-7.6906h1.1712z" style="white-space:pre"/>
+    </g>
+    </g>
+    </g>
+    </g>
+    </svg>
+    
+}
