53c8d08de456656c1d4a421030517f71748dc109
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-11-09 15:20:28 +0100

    Updated home.jsx

--- staff/johonas-sanchez/frontend/app_pruebas/views/Home.jsx
+++ staff/johonas-sanchez/frontend/app_pruebas/views/Home.jsx
@@ -1,6 +1,3 @@
-import PostList from "../components/postList";
-
-
 function Home(props) {
     console.log('Home')
 
@@ -120,12 +117,40 @@ function Home(props) {
             </form>
         </div> : null}
 
-        <PostList
-            posts={posts}
-            sessionUserId={sessionUserId}
-            handlePostLikeClick={handlePostLikeClick}
-            handlePostDeleteClick={handlePostDeleteClick}
-            handlePostSaveClick={handlePostSaveClick}
-        />
+        {posts !== null ? <div aria-label="Posts list" className="view">
+            {posts.toReversed().map(function (post) {
+                function handleBeforePostLikeClick() {
+                    handlePostLikeClick(post.id)
+                }
+
+                function handleBeforePostDeleteClick() {
+                    const confirmed = confirm('Delete post?')
+
+                    if (confirmed)
+                        handlePostDeleteClick(post.id)
+                }
+
+                function handleBeforePostSaveClick() {
+                    handlePostSaveClick(post.id)
+                }
+
+                return <article key={post.id} className="post">
+                    <h3>{post.author.name}</h3>
+
+                    <img className="post-image"
+                        src={post.image}
+                        alt={post.imageDescription}
+                        title={post.imageDescription} />
+
+                    <p>{post.text}</p>
+
+                    <button className="button" onClick={handleBeforePostLikeClick}>{(post.liked ? '❤️' : '🩶') + ' ' + post.likes.length + ' likes'}</button>
+
+                    <button className="button" onClick={handleBeforePostSaveClick}>{(post.saved ? '⭐️' : '✩')}</button>
+
+                    {post.author.id === sessionUserId ? <button className="button" onClick={handleBeforePostDeleteClick}>Delete</button> : null}
+                </article>
+            })}
+        </div> : null}
     </div>
-}               
+}             
