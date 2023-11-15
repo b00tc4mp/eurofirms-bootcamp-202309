b219799aa24ed669b6ead540f8c9f324b169333c
David Cano <webdelcano@gmail.com>
2023-11-15 23:58:19 +0100

    add functonality vite-react-david folder

--- staff/david-cano/frontend/pruebas/vite-react-david/src/App.jsx
+++ staff/david-cano/frontend/pruebas/vite-react-david/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Login from "./views/Login"
+import React from "react"
+import Logo from "./components/Logo"
+import Register from "./views/Register"
+import Home from "./views/Home"
 
 function App() {
-  const [count, setCount] = useState(0)
+    const viewState = React.useState('login')
+    const view = viewState[0]
+    const setView = viewState[1]
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    function handleShowRegister() {
+        setView('register')
+    }
+
+    function handleShowLogin() {
+        setView('login')
+    }
+
+    function handleShowHome() {
+        setView('home')
+    }
+
+    return <>
+        {<Logo />}
+
+        {view === 'login' ? <Login  onSuccess={handleShowHome} onRegisterClick={handleShowRegister} /> : null}
+
+        {view === 'register' ? <Register onSuccess={handleShowLogin}  onLoginClick = {handleShowLogin}/> : null}
+
+        {view === 'home' ? <Home onLogout={handleShowLogin} /> : null}
     </>
-  )
 }
 
 export default App
