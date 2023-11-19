a6e2917a5c9fd6e90b4261819594311214715414
Katherine Martel <katherine.martelv@gmail.com>
2023-11-19 23:53:39 +0100

    new app with vite

--- staff/katherine-martel/frontend/app/views/Home.jsx
+++ staff/katherine-martel/frontend/app/views/Home.jsx
@@ -9,6 +9,10 @@ function Home(props) {
 
     const timestamp = timestampState[1]
 
+    const savedState = React.useState(null)
+    const saved = savedState[0]
+    const setSaved = savedState[1]
+
     let name = null
 
     try {
@@ -64,12 +68,76 @@ function Home(props) {
 
     function handlePostLikeClick(postId) {
         try {
-            toggleLikePost(sessionUserId, postId)
+            deletePost(sessionUserId, postId)
+
+            if (view === 'saved') {
+                const saved = retrieveSavedPosts(sessionUserId)
+
+                setSaved(saved)
+
+                return
+            }
+
+            setTimestamp(Date.now())
+        } catch (error) {
+            alert(error.message)
+
+        }
+    }
+
+    function handlePostDeleteClick(postId) {
+        try {
+            deletePost(sessionUserId, postId)
+
+            if (view === 'saved') {
+                const saved = retrieveSavedPosts(sessionUserId)
+
+                setSaved(saved)
+
+                return
+            }
 
             setTimestamp(Date.now())
         } catch (error) {
             alert(error.message)
         }
+    }
+
+    function handlePostSaveClick(postId) {
+        try {
+            toggleSavePost(sessionUserId, postId)
+
+            if (view === 'saved') {
+                const saved = retrieveSavedPosts(sessionUserId)
+
+                setSaved(saved)
+
+                return
+            }
+
+            setTimestamp(Date.now())
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    function handleSavedClick(event) {
+        event.preventDefault()
+
+        try {
+            const saved = retrieveSavedPosts(sessionUserId)
+
+            setSaved(saved)
+            setView('saved')
+        } catch (error) {
+            alert(error.message)
+        }
+    }
+
+    function handleHomeClick(event) {
+        event.preventDefault()
+
+        setView(null)
     }
 
     return <div>
@@ -99,23 +167,74 @@ function Home(props) {
             </form>
         </div> : null}
 
-        {posts !== null ? <div id="posts-list" aria-label="Posts-list" className="view">
-            {posts.toReversed().map(function (post, index, posts) {
-                const liked = post.likes.includes(loggedInEmail)
-
+        {(view === null || view === 'new-post') && posts !== null ? <div aria-label="Posts list" className="view">
+            {posts.map(function (post) {
                 function handleBeforePostLikeClick() {
-                    handlePostLikeClick(postId)
+                    handlePostLikeClick(post.id)
+                }
 
+                function handleBeforePostDeleteClick() {
+                    const confirmed = confirm('Delete post?')
+
+                    if (confirmed)
+                        handlePostDeleteClick(post.id)
+                }
+
+                function handleBeforePostSaveClick() {
+                    handlePostSaveClick(post.id)
                 }
 
                 return <article key={post.id} className="post">
-                    <h3>{post.autor}</h3>
+                    <h3>{post.author.name}</h3>
+
                     <img className="post-image"
                         src={post.image}
                         alt={post.imageDescription}
                         title={post.imageDescription} />
+
                     <p>{post.text}</p>
-                    <button className="button" onClick={handleBeforePostLikeClick}>{(liked ? '❤️' : '🩶') + ' ' + post.likes.legth + 'likes'}</button>
+
+                    <button className="button" onClick={handleBeforePostLikeClick}>{(post.liked ? '❤️' : '🩶') + ' ' + post.likes.length + ' likes'}</button>
+
+                    <button className="button" onClick={handleBeforePostSaveClick}>{(post.saved ? '⭐️' : '✩')}</button>
+
+                    {post.author.id === sessionUserId ? <button className="button" onClick={handleBeforePostDeleteClick}>Delete</button> : null}
+                </article>
+            })}
+        </div> : null}
+
+        {view === 'saved' ? <div aria-label="Saved list" className="view">
+            {saved.map(function (post) {
+                function handleBeforePostLikeClick() {
+                    handlePostLikeClick(post.id)
+                }
+
+                function handleBeforePostDeleteClick() {
+                    const confirmed = confirm('Delete post?')
+
+                    if (confirmed)
+                        handlePostDeleteClick(post.id)
+                }
+
+                function handleBeforePostSaveClick() {
+                    handlePostSaveClick(post.id)
+                }
+
+                return <article key={post.id} className="post">
+                    <h3>{post.author.name}</h3>
+
+                    <img className="post-image"
+                        src={post.image}
+                        alt={post.imageDescription}
+                        title={post.imageDescription} />
+
+                    <p>{post.text}</p>
+
+                    <button className="button" onClick={handleBeforePostLikeClick}>{(post.liked ? '❤️' : '🩶') + ' ' + post.likes.length + ' likes'}</button>
+
+                    <button className="button" onClick={handleBeforePostSaveClick}>{(post.saved ? '⭐️' : '✩')}</button>
+
+                    {post.author.id === sessionUserId ? <button className="button" onClick={handleBeforePostDeleteClick}>Delete</button> : null}
                 </article>
             })}
         </div> : null}
