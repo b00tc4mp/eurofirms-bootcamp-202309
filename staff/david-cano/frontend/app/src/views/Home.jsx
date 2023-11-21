41b54cba4b6abc921393bf0e853b76c0c70999db
David Cano <webdelcano@gmail.com>
2023-11-21 14:10:55 +0100

    add AllPosts.jsx, MyPosts.jsx, retrieveMyPosts.js and retrieveSavedPosts.js

--- staff/david-cano/frontend/app/src/views/Home.jsx
+++ staff/david-cano/frontend/app/src/views/Home.jsx
@@ -1,28 +1,21 @@
 import { useState } from 'react'
 
 import retrieveUser from '../logic/retrieveUser'
-import retrievePosts from '../logic/retrievePosts'
-import createNewPost from '../logic/createNewPost'
-import retrieveSavedPosts from '../logic/retrieveSavedPosts'
-
 
 import Button from '../components/Button'
 import Link from '../components/Link'
-import Field from '../components/Field'
-import Form from '../components/Form'
 import Container from '../components/Container'
-import Posts from '../components/Posts'
+import MyPosts from '../components/MyPosts'
+import SavedPosts from '../components/SavedPosts'
+import AllPosts from '../components/AllPosts'
+import NewPost from '../components/NewPost'
 
 import Logo from '../components/Logo'
 
 function Home(props) {
+    console.log('Home')
 
     const [view, setView] = useState(null)
-
-    const [timestamp, setTimestamp] = useState(null)
-
-    const [saved, setSaved] = useState(null)
-    // TODO const [saved, setSaved] = React.useState(null)
 
     let name = null
 
@@ -30,14 +23,6 @@ function Home(props) {
         const user = retrieveUser(window.sessionUserId)
 
         name = user.name
-    } catch (error) {
-        alert(error.message)
-    }
-
-    let posts = null
-
-    try {
-        posts = retrievePosts(window.sessionUserId)
     } catch (error) {
         alert(error.message)
     }
@@ -56,62 +41,14 @@ function Home(props) {
         setView(null)
     }
 
-    function handleNewPostSubmit(event) {
-        event.preventDefault()
-
-        const imageInput = event.target.querySelector('#image-field')
-        const imageDescriptionInput = event.target.querySelector('#image-description-field')
-        const textInput = event.target.querySelector('#text-field')
-
-        const image = imageInput.value
-        const imageDescription = imageDescriptionInput.value
-        const text = textInput.value
-
-        try {
-            createNewPost(window.sessionUserId, image, imageDescription, text)
-
-            setView(null)
-        } catch (error) {
-            alert(error.message)
-        }
-    }
-
-    function refreshPosts() {
-        if (view === null || view === 'new-post')
-            setTimestamp(Date.now())
-        else if (view === 'saved')
-            try {
-                const saved = retrieveSavedPosts(window.sessionUserId)
-
-                setSaved(saved)
-            } catch (error) {
-                alert(error.message)
-            }
-    }
-
-    function handlePostLikeClick(postId) {
-        refreshPosts()
-    }
-
-    function handlePostDeleteClick(postId) {
-        refreshPosts()
-    }
-
-    function handlePostSaveClick(postId) {
-        refreshPosts()
+    function handleNewPostSubmit() {
+        setView(null)
     }
 
     function handleSavedClick(event) {
         event.preventDefault()
 
-        try {
-            const saved = retrieveSavedPosts(window.sessionUserId)
-
-            setSaved(saved)
-            setView('saved')
-        } catch (error) {
-            alert(error.message)
-        }
+        setView('saved')
     }
 
     function handleHomeClick(event) {
@@ -120,38 +57,36 @@ function Home(props) {
         setView(null)
     }
 
+    function handleMyPostsClick(event) {
+        event.preventDefault()
+
+        setView('my-posts')
+    }
+
 
     return <Container>
-        <header className="header" aria-label="Header">
-            <Link onClick={handleHomeClick}><Logo /></Link>
-            <span id="user-name-span" aria-label="User name">{name}</span>
-            <Button title="New post" aria-label="New post (+)" onClick={handleNewPostClick}>+</Button>
-            <Link onClick={handleSavedClick}>Saved</Link>
-            <Button onClick={handleLogoutClick}>Logout</Button>
-        </header>
+    <header className="header" aria-label="Header">
+        <Link onClick={handleHomeClick}><Logo /></Link>
 
-        {view === 'new-post' ?
-            <Container align = 'center'>
-                <h2>New post</h2>
+        <span aria-label="User name">{name}</span>
 
-                <Form onSubmit={handleNewPostSubmit}>
-                    <Field type="url" id="image-field" required >Image</Field>
-                    
-                    <Field type="text" id="image-description-field" required >Image Description</Field>
+        <Button title="New post" aria-label="New post (+)" onClick={handleNewPostClick}>+</Button>
 
-                    <Field type="text" id="text-field" required >Text</Field>
+        <Link onClick={handleSavedClick}>Saved</Link>
 
-                    <Button type="submit">Post</Button>
-                    <Button onClick={handleNewPostCancelClick}>Cancel</Button>
-                </Form>
-            </Container> : null}
+        <Link onClick={handleMyPostsClick}>My posts</Link>
 
-        {(view === null || view === 'new-post') && posts !== null ? <Posts posts={posts} onLikeClick={handlePostLikeClick} onSaveClick={handlePostSaveClick} onDeleteClick={handlePostDeleteClick} />
-            : null }
+        <Button onClick={handleLogoutClick}>Logout</Button>
+    </header>
 
-        {view === 'saved' && saved !== null ? <Posts posts={saved} onLikeClick={handlePostLikeClick} onSaveClick={handlePostSaveClick} onDeleteClick={handlePostDeleteClick} />
-            : null}
-    </Container>
+    {view === 'new-post' ? <NewPost onNewPostSubmit={handleNewPostSubmit} onNewPostCancelClick={handleNewPostCancelClick} /> : null}
+
+    {view === null || view === 'new-post' ? <AllPosts /> : null}
+
+    {view === 'saved' ? <SavedPosts /> : null}
+
+    {view === 'my-posts' ? <MyPosts /> : null}
+</Container>
 }
 
 export default Home
