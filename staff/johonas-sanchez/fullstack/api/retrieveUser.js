a254fbb79da9d07a6aef6aefec2e3f0f97ebd16b
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-11-22 14:34:52 +0100

    update api code

--- staff/johonas-sanchez/fullstack/api/retrieveUser.js
+++ staff/johonas-sanchez/fullstack/api/retrieveUser.js
@@ -1,1 +1,23 @@
-// TODO implement a logic that retrieves a specific user by id
+const loadUsers = require('./helpers/loadUsers')
+
+function retrieveUser(userId, callback) {
+    // TODO validate input
+
+    loadUsers(function (error, users) {
+        if (error) {
+            callback(error)
+
+            return
+        }
+
+        const user = users.find(function (user) {
+            return user.id === userId
+        })
+
+        if (!user) throw new Error('user not found')
+
+        callback(null, user)
+    })
+}
+
+module.exports = retrieveUser
