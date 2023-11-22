958b61e85ddf3f799062c703fd3bf94052251387
Manuel Barzi <manuelbarzi@gmail.com>
2023-11-22 10:46:26 +0100

    add retrieve user sync implementation

--- staff/manuel-barzi/fullstack/api/retrieveUser.js
+++ staff/manuel-barzi/fullstack/api/retrieveUser.js
@@ -1,1 +1,30 @@
-// TODO implement a logic that retrieves a specific user by id
+const fs = require('fs')
+
+function retrieveUser(userId) {
+    const text = fs.readFileSync('./users.csv').toString()
+
+    const lines = text.split('\n')
+
+    const users = []
+
+    for (let i = 1; i < lines.length; i++) {
+        const line = lines[i]
+
+        const values = line.split(',')
+
+        const [id, name, email, password] = values
+
+        const user = { id, name, email, password }
+
+        users.push(user)
+    }
+
+    const user = users.find(function (user) {
+        return user.id === userId
+    })
+
+    return user
+}
+
+// export default retrieveUser
+module.exports = retrieveUser
