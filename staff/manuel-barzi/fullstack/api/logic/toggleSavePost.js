0f68224e63860638452b8d9bd7f11e0d2523e398
Manuel Barzi <manuelbarzi@gmail.com>
2023-11-28 12:02:25 +0100

    add retrieve my posts, retrieve saved posts, and toggle save post logics with tests

---
+++ staff/manuel-barzi/fullstack/api/logic/toggleSavePost.js
@@ -0,0 +1,32 @@
+const { validateText, validateFunction } = require('./helpers/validators')
+
+const { User, Post } = require('../data/models')
+
+function toggleSavePost(userId, postId, callback) {
+    validateText(userId, 'userId')
+    validateText(userId, 'userId')
+    validateFunction(callback, 'callback')
+
+    User.findById(userId)
+        .then(user => {
+            if (!user) {
+                callback(new Error('User not found'))
+
+                return
+            }
+
+            const index = user.saved.findIndex(postObjectId => postObjectId.toString() === postId)
+
+            if (index < 0)
+                user.saved.push(postId)
+            else
+                user.saved.splice(index, 1)
+
+            user.save()
+                .then(() => callback(null))
+                .catch(error => callback(error))
+        })
+        .catch(error => callback(error))
+}
+
+module.exports = toggleSavePost
