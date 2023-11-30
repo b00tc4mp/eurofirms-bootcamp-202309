4095d773d425bc190e63c88ad6585ad199aa3daa
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-11-30 17:03:25 +0100

    add retrievePosts toggleLikePost deletePost retrieveUser retrieveMyPosts toggleSavePost retrieveSavedPosts endpoints #43

--- staff/johonas-sanchez/fullstack/api/index.js
+++ staff/johonas-sanchez/fullstack/api/index.js
@@ -1,115 +1,246 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express")
+const mongoose = require("mongoose")
 
-const registerUser = require('./logic/registerUser')
-const authenticateUser = require('./logic/authenticateUser')
-const createPost = require('./logic/createPost')
-const retrievePosts = require('./logic/retrievePosts')
+const registerUser = require("./logic/registerUser")
+const authenticateUser = require("./logic/authenticateUser")
+const createPost = require("./logic/createPost")
+const retrievePosts = require("./logic/retrievePosts")
+const toggleLikePost = require("./logic/toggleLikePost")
+const deletePost = require("./logic/deletePost")
+const retrieveUser = require("./logic/retrieveUser")
+const retrieveMyPosts = require("./logic/retrieveMyPosts")
+const toggleSavePost = require("./logic/toggleSavePost")
+const retrieveSavedPosts = require("./logic/retrieveSavedPosts")
 
-mongoose.connect('mongodb://127.0.0.1/api')
-    .then(() => {
-        const api = express()
+mongoose.connect("mongodb://127.0.0.1/api").then(() => {
+  const api = express();
 
-        api.get('/helloworld', (req, res) => {
-            res.send('Hello, World!')
-        })
+  api.get("/helloworld", (req, res) => {
+    res.send("Hello, World!");
+  });
 
-        api.get('/holamundo', (req, res) => {
-            res.send('Hola, Mundo!')
-        })
+  api.get("/holamundo", (req, res) => {
+    res.send("Hola, Mundo!");
+  });
 
-        api.get('/hello', (req, res) => {
-            const name = req.query.name
+  api.get("/hello", (req, res) => {
+    const name = req.query.name;
 
-            res.send(`Hello, ${name}!`)
-        })
+    res.send(`Hello, ${name}!`);
+  });
 
-        const jsonBodyParser = express.json()
+  const jsonBodyParser = express.json();
 
-        api.post('/users', jsonBodyParser, (req, res) => {
-            const body = req.body
+  api.post("/users", jsonBodyParser, (req, res) => {
+    const body = req.body;
 
-            const { name, email, password } = body
+    const { name, email, password } = body;
 
-            try {
-                registerUser(name, email, password, error => {
-                    if (error) {
-                        res.status(400).json({ error: error.message })
+    try {
+      registerUser(name, email, password, (error) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
 
-                        return
-                    }
+          return;
+        }
 
-                    res.status(201).send()
-                })
-            } catch (error) {
-                res.status(400).json({ error: error.message })
-            }
-        })
+        res.status(201).send();
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
-        // TODO implement authenticate endpoint
+  // Implement authenticate endpoint
 
-        api.post('/users/authenticate', jsonBodyParser, (req, res) => {
-            const body = req.body
+  api.post("/users/authenticate", jsonBodyParser, (req, res) => {
+    const body = req.body;
 
-            const { email, password } = body
+    const { email, password } = body;
 
-            try {
-                authenticateUser(email, password, (error, userId) => {
-                    if (error) {
-                        res.status(400).json({ error: error.message })
+    try {
+      authenticateUser(email, password, (error, userId) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
 
-                        return
-                    }
+          return;
+        }
 
-                    res.json(userId)
-                })
-            } catch (error) {
-                res.status(400).json({ error: error.message })
-            }
-        })
+        res.json(userId);
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
-        // Implement createPost endpoint
+  // Implement createPost endpoint
 
-        api.post('/posts', jsonBodyParser, (req, res) => {
-            const body = req.body
+  api.post("/posts", jsonBodyParser, (req, res) => {
+    const userId = req.headers.authorization.slice(7);
+    const body = req.body;
 
-            const { userId, image, imageDescription, text } = body
+    const { image, imageDescription, text } = body;
 
-            try {
-                createPost(userId, image, imageDescription, text, error => {
-                    if (error) {
-                        res.status(400).json({ error: error.message })
+    try {
+      createPost(userId, image, imageDescription, text, (error) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
 
-                        return
-                    }
+          return;
+        }
 
-                    res.status(201).send()
-                })
-            } catch (error) {
-                res.status(400).json({ error: error.message })
-            }
-        })
+        res.status(201).send();
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+  // Implement retrievePosts endpoint
+
+  api.get("/posts", (req, res) => {
+    const userId = req.headers.authorization.slice(7); // Para que nos quede el userId sin el Bearer
+
+    try {
+      retrievePosts(userId, (error, posts) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
+
+          return;
+        }
+
+        res.json(posts);
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+  // Implement toggleLikePost endpoint
+
+  api.patch("/posts/:postId/likes", (req, res) => {
+    const userId = req.headers.authorization.slice(7);
+    const postId = req.params.postId;
+
+    try {
+      toggleLikePost(userId, postId, (error) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
+
+          return;
+        }
+
+        res.status(204).send();
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+  // Implement deletePost endpoint
+
+  api.delete("/posts/:postId", (req, res) => {
+    const userId = req.headers.authorization.slice(7);
+    const postId = req.params.postId;
+
+    try {
+      deletePost(userId, postId, (error) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
+
+          return;
+        }
+
+        res.status(204).send();
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+  // Implement retrieveUser endpoint
+
+  api.get("/users", (req, res) => {
+    const userId = req.headers.authorization.slice(7);
+
+    try {
+      retrieveUser(userId, (error, user) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
+
+          return;
+        }
+
+        res.json(user);
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+
+  // Implement retrieveMyPosts endpoint
+
+  api.get("/posts/user", (req, res) => {
+    const userId = req.headers.authorization.slice(7);
+
+    try {
+      retrieveMyPosts(userId, (error, posts) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
+
+          return;
+        }
+
+        res.json(posts);
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
+  
+
+  // Implement toggleSavePost endpoint
+
+  api.patch("/posts/:postId/saved", (req, res) => {
+    const userId = req.headers.authorization.slice(7);
+    const postId = req.params.postId;
+
+    try {
+      toggleSavePost(userId, postId, (error) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
+
+          return;
+        }
+
+        res.status(204).send();
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
 
-        // Implement retrievePosts endpoint
+  // Implement retrieveSavedPosts endpoint
 
-        api.get('/posts', (req, res) => {
-            const userId = req.headers.authorization.slice(7) // Para que nos quede el sin el Bearer
+  api.get("/posts/saved", (req, res) => {
+    const userId = req.headers.authorization.slice(7);
 
-            try {
-                retrievePosts(userId, (error, posts) => {
-                    if (error) {
-                        res.status(400).json({ error: error.message })
+    try {
+      retrieveSavedPosts(userId, (error, posts) => {
+        if (error) {
+          res.status(400).json({ error: error.message });
 
-                        return
-                    }
+          return;
+        }
 
-                    res.json(posts)
-                })
-            } catch (error) {
-                res.status(400).json({ error: error.message })
-            }
-        })
+        res.json(posts);
+      });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
+    }
+  });
 
-        api.listen(4000, () => console.log('API listening on port 4000'))
-    })
+
+  api.listen(4000, () => console.log("API listening on port 4000"));
+});
