14a1ee3e65567fed18111ebfee529fae5adb9d9a
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-04 16:56:54 +0100

    add retrieve posts

---
+++ staff/johonas-sanchez/fullstack/api/test/createPost.test.js
@@ -0,0 +1,22 @@
+const req = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer 656ddc296fc21579c1675d6e'
+    },
+    body: JSON.stringify({ image: 'https://wallpapers.com/images/hd/cute-minion-happy-bob-v1x9tfcn0rznkvvd.jpg', imageDescription: 'bat image 1', text: 'hello iron 1 fetch' })
+}
+
+fetch('http://localhost:4000/posts', req)
+    .then(res => {
+        if (!res.ok) {
+            res.json()
+                .then(body => console.error(body))
+                .catch(error => console.error(error))
+
+            return
+        }
+
+        console.log('created')
+    })
+    .catch(error => console.error(error))
---
+++ staff/johonas-sanchez/fullstack/api/test/retrievePosts.test.js
@@ -0,0 +1,22 @@
+const req = {
+   method: "GET",
+   headers: {
+      Authorization: "Bearer 656ddc296fc21579c1675d6e",
+   },
+}
+
+fetch("http://localhost:4000/posts", req)
+   .then((res) => {
+      if (!res.ok) {
+         res.json()
+            .then((body) => console.error(body))
+            .catch((error) => console.error(error))
+
+         return
+      }
+
+      res.json()
+         .then((posts) => console.log(posts))
+         .catch((error) => console.error(error))
+   })
+   .catch((error) => console.error(error))
