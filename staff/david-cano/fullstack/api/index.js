bce5f8abe51d7ff2aa4f0036a117b661595361c5
David Cano <webdelcano@gmail.com>
2023-12-04 04:27:01 +0100

    Implement endpoints in API #46

--- staff/david-cano/fullstack/api/index.js
+++ staff/david-cano/fullstack/api/index.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose')
 
 const registerUser = require('./logic/registerUser')
 const authenticateUser = require('./logic/authenticateUser')
+const retrieveUser = require('./logic/retrieveUser')
+const createPost = require("./logic/createPost")
+const retrievePosts = require("./logic/retrievePosts")
+const toggleLikePost = require("./logic/toggleLikePost")
+const retrieveSavedPosts = require("./logic/retrieveSavedPosts")
+const toggleSavePost = require("./logic/toggleSavePost")
+const retrieveMyPosts = require("./logic/retrieveMyPosts")
+const deletePost = require("./logic/deletePost")
 
 mongoose.connect('mongodb://127.0.0.1/api')
     .then(() => {
@@ -45,22 +53,23 @@ mongoose.connect('mongodb://127.0.0.1/api')
         })
 
         // TODO implement authenticate endpoint
+
         api.post('/users/authenticate', jsonBodyParser, (req, res) => {
             const body = req.body
-            
+
             const { email, password } = body
 
             try {
                 authenticateUser(email, password, (error, userId) => {
                     if (error) {
-                        res.status(400).json({ error: error.message})
+                        res.status(400).json({ error: error.message })
                         return
                     }
 
                     res.json(userId)
                 })
             } catch (error) {
-                res.status(400).json({ error: error.message})
+                res.status(400).json({ error: error.message })
             }
         })
 
@@ -70,16 +79,148 @@ mongoose.connect('mongodb://127.0.0.1/api')
             try {
                 retrieveUser(userId, (error, user) => {
                     if (error) {
-                        res.status(400).json({ error: error.message})
+                        res.status(400).json({ error: error.message })
                         return
                     }
 
                     res.json(user)
                 })
             } catch (error) {
-                res.status(400).json({ error: error.message})
+                res.status(400).json({ error: error.message })
             }
         })
+
+        api.post("/posts", jsonBodyParser, (req, res) => {
+            const userId = req.headers.authorization.slice(7);
+            const body = req.body;
+
+            const { image, imageDescription, text } = body;
+
+            try {
+                createPost(userId, image, imageDescription, text, (error) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.status(201).send();
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        api.get("/posts", (req, res) => {
+            const userId = req.headers.authorization.slice(7); // Para que nos quede el userId sin el Bearer
+
+            try {
+                retrievePosts(userId, (error, posts) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.json(posts);
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        api.patch("/posts/:postId/likes", (req, res) => {
+            const userId = req.headers.authorization.slice(7);
+            const postId = req.params.postId;
+
+            try {
+                toggleLikePost(userId, postId, (error) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.status(204).send();
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        api.get("/posts/saved", (req, res) => {
+            const userId = req.headers.authorization.slice(7);
+
+            try {
+                retrieveSavedPosts(userId, (error, posts) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.json(posts);
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        api.patch("/posts/:postId/saved", (req, res) => {
+            const userId = req.headers.authorization.slice(7);
+            const postId = req.params.postId;
+
+            try {
+                toggleSavePost(userId, postId, (error) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.status(204).send();
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        api.get("/posts/user", (req, res) => {
+            const userId = req.headers.authorization.slice(7);
+
+            try {
+                retrieveMyPosts(userId, (error, posts) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.json(posts);
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        api.delete("/posts/:postId", (req, res) => {
+            const userId = req.headers.authorization.slice(7);
+            const postId = req.params.postId;
+
+            try {
+                deletePost(userId, postId, (error) => {
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+
+                        return;
+                    }
+
+                    res.status(204).send();
+                });
+            } catch (error) {
+                res.status(400).json({ error: error.message });
+            }
+        });
 
         api.listen(4000, () => console.log('API listening on port 4000'))
     })
--- staff/david-cano/fullstack/pruebas/api4-david-copy/index.js
+++ staff/david-cano/fullstack/pruebas/api4-david-copy/index.js
@@ -1,70 +1,109 @@
-const express = require('express')
-const mongoose = require('mongoose')
+// Importa los módulos necesarios para crear la aplicación web y trabajar con MongoDB.
+const express = require('express');
+const mongoose = require('mongoose');
 
-const registerUser = require('./logic/registerUser')
+// Importa las funciones de lógica para el registro, autenticación y recuperación de usuarios.
+const registerUser = require('./logic/registerUser');
 const authenticateUser = require('./logic/authenticateUser');
+const retrieveUser = require('./logic/retrieveUser');
 
-
+// Conecta a la base de datos MongoDB.
 mongoose.connect('mongodb://127.0.0.1/api')
     .then(() => {
-        const api = express()
+        // Crea una instancia de la aplicación Express.
+        const api = express();
 
+        // Define rutas para endpoints básicos.
         api.get('/helloworld', (req, res) => {
-            res.send('Hello, World!')
-        })
+            res.send('Hello, World!');
+        });
 
         api.get('/holamundo', (req, res) => {
-            res.send('Hola, Mundo!')
-        })
+            res.send('Hola, Mundo!');
+        });
 
         api.get('/hello', (req, res) => {
-            const name = req.query.name
+            // Extrae el parámetro 'name' de la consulta.
+            const name = req.query.name;
+            res.send(`Hello, ${name}!`);
+        });
 
-            res.send(`Hello, ${name}!`)
-        })
+        // Configura el middleware para analizar el cuerpo de las solicitudes en formato JSON.
+        const jsonBodyParser = express.json();
 
-        const jsonBodyParser = express.json()
-
+        // Define una ruta para el registro de usuarios.
         api.post('/users', jsonBodyParser, (req, res) => {
-            const body = req.body
-
-            const { name, email, password } = body
-
-            try {
-                registerUser(name, email, password, error => {
-                    if (error) {
-                        res.status(400).json({ error: error.message })
-
-                        return
-                    }
-
-                    res.status(201).send()
-                })
-            } catch (error) {
-                res.status(400).json({ error: error.message })
-            }
-        })
-
-        // TODO implement authenticate endpoint
-        api.post('/authenticate', jsonBodyParser, (req, res) => {
+            // Extrae el cuerpo de la solicitud.
             const body = req.body;
-        
-            const { email, password } = body;
-        
+            // Extrae campos específicos del cuerpo de la solicitud.
+            const { name, email, password } = body;
+
             try {
-                authenticateUser(email, password, error => {
+                // Llama a la función 'registerUser' con los datos proporcionados.
+                registerUser(name, email, password, error => {
+                    // Maneja errores devueltos por 'registerUser'.
                     if (error) {
                         res.status(400).json({ error: error.message });
                         return;
                     }
-        
+
+                    // En caso de éxito, devuelve un código de estado 201 (Creado).
                     res.status(201).send();
                 });
             } catch (error) {
+                // Captura y maneja cualquier error que ocurra durante el proceso de registro.
                 res.status(400).json({ error: error.message });
             }
         });
-        
 
-        api.listen(4000, () => console.log('API listening on port 4000'))
-    })
+        // Define la ruta para la autenticación de usuarios.
+        api.post('/users/authenticate', jsonBodyParser, (req, res) => {
+            // Extrae el cuerpo de la solicitud.
+            const body = req.body;
+            // Extrae campos específicos del cuerpo de la solicitud.
+            const { email, password } = body;
+
+            try {
+                // Llama a la función 'authenticateUser' con el email y la contraseña proporcionados.
+                authenticateUser(email, password, (error, userId) => {
+                    // Maneja errores devueltos por 'authenticateUser'.
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+                        return;
+                    }
+
+                    // Si la autenticación es exitosa, devuelve el ID del usuario en formato JSON.
+                    res.json(userId);
+                });
+            } catch (error) {
+                // Captura y maneja cualquier error que ocurra durante el proceso de autenticación.
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        // Define una ruta para recuperar la información de un usuario.
+        api.post('/users', (req, res) => {
+            // Extrae el ID del usuario del encabezado 'Authorization'.
+            const userId = req.headers.authorization.slice(7);
+
+            try {
+                // Llama a la función 'retrieveUser' con el ID del usuario.
+                retrieveUser(userId, (error, user) => {
+                    // Maneja errores devueltos por 'retrieveUser'.
+                    if (error) {
+                        res.status(400).json({ error: error.message });
+                        return;
+                    }
+
+                    // Si la recuperación es exitosa, devuelve la información del usuario en formato JSON.
+                    res.json(user);
+                });
+            } catch (error) {
+                // Captura y maneja cualquier error que ocurra durante el proceso de recuperación de usuario.
+                res.status(400).json({ error: error.message });
+            }
+        });
+
+        // Inicia la aplicación Express en el puerto 4000.
+        api.listen(4000, () => console.log('API listening on port 4000'));
+    });
