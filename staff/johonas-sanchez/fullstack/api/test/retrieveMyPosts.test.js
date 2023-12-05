b73a8be9a4f703d149ffec6edcf1c227f905ef1d
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-05 11:20:52 +0100

    update fetch gramatical error

--- staff/johonas-sanchez/fullstack/api/test/retrieveMyPosts.test.js
+++ staff/johonas-sanchez/fullstack/api/test/retrieveMyPosts.test.js
@@ -1,22 +1,22 @@
 const req = {
-    method: "GET",
+    method: 'GET',
     headers: {
-       Authorization: "Bearer 656ddc296fc21579c1675d6e",
+       Authorization: 'Bearer 656ddc296fc21579c1675d6e',
     },
  }
  
- fetch("http://localhost:4000/posts/user", req)
+ fetch('http://localhost:4000/posts/user', req)
     .then(res => {
        if (!res.ok) {
           res.json()
-             .then((body) => console.error(body))
-             .catch((error) => console.error(error))
+             .then(body => console.error(body))
+             .catch(error => console.error(error))
  
           return
        }
  
        res.json()
-          .then((posts) => console.log(posts))
-          .catch((error) => console.error(error))
+          .then(posts => console.log(posts))
+          .catch(error => console.error(error))
     })
     .catch((error) => console.error(error))
