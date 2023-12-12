0d32cb820b4e232132842e35ace06e9c7cc0f184
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-12 15:49:53 +0100

    work in userProfile form #76

--- staff/johonas-sanchez/fullstack/app/src/components/UserProfile.jsx
+++ staff/johonas-sanchez/fullstack/app/src/components/UserProfile.jsx
@@ -25,27 +25,35 @@ return<><Container>
     <Button onClick={handleChangePasswordClick}>Change Password</Button>
     </Container>
 
-    {view === 'change-email' && 
-    <Container>
+    {view === 'change-password' && 
+    <Container className="mt-7">
         <Form>
-            <Field>Change Email</Field>
-            <Field></Field>
-            <Field></Field>
-            <Button>Enviar</Button>
-            <Button onClick={handleCancelClick}>Cancelar</Button>
+            <Field type="password" id="old-password" required>Old Password</Field>
+
+            <Field type="password" id="new-password" required>New password</Field>
+
+            <Field type="password" id="repeat-new-password" required>Repeat new password</Field>
+
+            <Button type="submit">Actualizar</Button>
+            <Button onClick={handleCancelClick}>Cancel</Button>
         </Form>
     </Container> }
 
-    {view === 'change-password' && 
-    <Container>
-        <Form>
-            <Field>Change Password</Field>
-            <Field></Field>
-            <Field></Field>
-            <Button>Enviar</Button>
-            <Button onClick={handleCancelClick}>Cancelar</Button>
-        </Form>
-    </Container> }
+    {view === 'change-email' && 
+    <Container className="mt-7">
+    <Form>
+        <Field type="password" id="password" required>Password</Field>
+
+        <Field type="email" id="old-email" required>Old email</Field>
+
+        <Field type="email" id="new-email" required>New email</Field>
+
+        <Field type="email" id="repeat-new-email" required>Repeat new email</Field>
+
+        <Button type="submit">Actualizar</Button>
+        <Button onClick={handleCancelClick}>Cancel</Button>
+    </Form>
+</Container> }
 
     </>
 }
