4b083eada1cf8ef16a9f5d40a0c7b76d60003421
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-14 16:48:52 +0100

    add token expired date #79

--- staff/johonas-sanchez/fullstack/api/index.js
+++ staff/johonas-sanchez/fullstack/api/index.js
@@ -76,7 +76,7 @@ mongoose.connect("mongodb://127.0.0.1/api").then(() => {
                return
             }
 
-            const token = jwt.sign({ sub: userId }, "es posible que pronto sea abuelo" ) // Ponemos la fecha de expiración del token
+            const token = jwt.sign({ sub: userId }, "es posible que pronto sea abuelo", { expiresIn: '10s' } ) // Ponemos la fecha de expiración del token
 
             res.json(token)
          })
