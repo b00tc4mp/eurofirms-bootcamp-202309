70299469990e8796c5870a67968bb110646739b8
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-28 12:43:20 +0100

    add register page and isUserLoggedIn logic #97

--- staff/johonas-sanchez/project/app/src/App.jsx
+++ staff/johonas-sanchez/project/app/src/App.jsx
@@ -2,15 +2,60 @@ import { Routes, Route, useNavigate, Navigate } from "react-router-dom"
 import './App.css'
 import Login from "./pages/Login"
 import Home from "./pages/Home"
+import Register from "./pages/Register"
+
+import logic from "./logic"
 
 function App() {
 
+  function handleRegisterShow() {
+    navigate("/register")
+ }
+
+  function handleLoginShow() {
+    navigate("/login")
+ }
+
+ function handleHomeShow() {
+    navigate("/")
+ }
+
   return (
     <>
-    <Routes>
+    {/* <Routes>
         <Route path="/login" element={ <Login /> } />
         <Route path="/" element={ <Home /> } />
-      </Routes>
+      </Routes> */}
+               <Routes>
+            <Route
+               path="/login"
+               element={
+                  logic.isUserLoggedIn() ? (
+                     <Navigate to="/" />
+                  ) : (
+                     <Login onSuccess={handleHomeShow}  />
+                  )
+               }
+            />
+
+            <Route
+               path="/register"
+               element={
+                  logic.isUserLoggedIn() ? (
+                     <Navigate to="/" />
+                  ) : (
+                     <Register onSuccess={handleLoginShow} onLoginClick={handleLoginShow} />
+                  )
+               }
+            />
+
+            <Route
+               path="/*"
+               element={logic.isUserLoggedIn() ? <Home onLogout={handleLoginShow} /> : <Navigate to="/login" />}
+            />
+
+
+         </Routes>
     </>
   )
 }
---
+++ staff/johonas-sanchez/project/app/src/pages/Register.jsx
@@ -0,0 +1,9 @@
+function Register() {
+   console.log("Register")
+
+   return (
+         <h1>Register</h1>
+   )
+}
+
+export default Register
