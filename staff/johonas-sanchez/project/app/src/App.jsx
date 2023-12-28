9ec4e824deb41f407fd604ba8f3c6348bc21afd3
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-28 12:22:37 +0100

    add route to app pages #97

--- staff/johonas-sanchez/project/app/src/App.jsx
+++ staff/johonas-sanchez/project/app/src/App.jsx
@@ -1,11 +1,16 @@
-
+import { Routes, Route, useNavigate, Navigate } from "react-router-dom"
 import './App.css'
+import Login from "./pages/Login"
+import Home from "./pages/Home"
 
 function App() {
 
   return (
     <>
-      <h1>Hola</h1>
+    <Routes>
+        <Route path="/login" element={ <Login /> } />
+        <Route path="/" element={ <Home /> } />
+      </Routes>
     </>
   )
 }
