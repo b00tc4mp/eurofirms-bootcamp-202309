0a0e4fb3d3c90480b4b4320020d9a8f04578085b
Johonas Sanchez <johonas.s.nava@gmail.com>
2023-12-29 15:52:05 +0100

    create new projectTest db; update authenticateUser.test, registerUser.test, retrieveUser.test and run tests #97

--- staff/johonas-sanchez/project/api/logic/authenticateUser.test.js
+++ staff/johonas-sanchez/project/api/logic/authenticateUser.test.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose')
 
 const authenticateUser = require('./authenticateUser')
 
-mongoose.connect('mongodb://127.0.0.1:27017/test')
+mongoose.connect('mongodb://127.0.0.1:27017/projectTest')
     .then(() => {
         try {
-            authenticateUser('wonder@woman.com', '123123123')
+            authenticateUser('manolo@garcia.com', '123123123')
             .then(userId => console.log('user authenticated', userId))
             .catch(error => console.error(error))
             
--- staff/johonas-sanchez/project/api/logic/registerUser.test.js
+++ staff/johonas-sanchez/project/api/logic/registerUser.test.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose")
 
 const registerUser = require("./registerUser")
 
-mongoose.connect("mongodb://127.0.0.1:27017/test")
+mongoose.connect("mongodb://127.0.0.1:27017/projectTest")
 .then(() => {
    try {
-      registerUser("Wonder Woman", "wonder@woman.com", "123123123")
+      registerUser("Manolo García", "manolo@garcia.com", "123123123")
          .then(() => console.log("user registered"))
          .catch((error) => console.error(error))
    } catch (error) {
