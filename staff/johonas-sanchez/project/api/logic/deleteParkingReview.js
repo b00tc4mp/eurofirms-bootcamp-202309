5350ffd1711c44502ea22c31eed522ab6a84c17c
Johonas Sanchez <johonas.s.nava@gmail.com>
2024-01-02 16:13:19 +0100

    add deleteParkingReview #97

---
+++ staff/johonas-sanchez/project/api/logic/deleteParkingReview.js
@@ -0,0 +1,49 @@
+const { validate } = require('./helpers')
+const { User, Review } = require('../data/models')
+const { NotFoundError, ClearanceError, SystemError } = require('./errors')
+
+function deleteParkingReview(userId, reviewId, callback) {
+    validate.id(userId, 'user id')
+    validate.id(reviewId, 'review id')
+    validate.function(callback, 'callback')
+
+    User.findById(userId)
+        .then(user => {
+            if (!user) {
+                callback(new NotFoundError('user not found'))
+
+                return
+            }
+
+            Review.findById(reviewId)
+                .then(review => {
+                    if (!review) {
+                        callback(new NotFoundError('review not found'))
+
+                        return
+                    }
+
+                    if (review.author.toString() !== userId) {
+                        callback(new ClearanceError('review does not belong to user'))
+
+                        return
+                    }
+
+                    Review.deleteOne({ _id: reviewId })
+                        .then(result => {
+                            if(result.deletedCount === 0) {
+                                callback(new SystemError('review can not be deleted'))
+
+                                return
+                            }
+                            
+                            callback(null)
+                        })
+                        .catch(error => callback(new SystemError(error.message)))
+                })
+                .catch(error => callback(new SystemError(error.message)))
+        })
+        .catch(error => callback(new SystemError(error.message)))
+}
+
+module.exports = deleteParkingReview
--- staff/johonas-sanchez/project/api/logic/deleteParkingReview.test.js
+++ staff/johonas-sanchez/project/api/logic/deleteParkingReview.test.js
@@ -1,17 +1,17 @@
 const mongoose = require('mongoose')
-const deleteParking = require('./deleteParking')
+const deleteParkingReview = require('./deleteParkingReview')
 
 mongoose.connect('mongodb://127.0.0.1:27017/projectTest')
     .then(() => {
         try {
-            deleteParking('6593d3059ea31c753328c2fd', '6593e2ef303fba11492e51f9', error => {
+            deleteParkingReview('6593d3059ea31c753328c2fd', '6594222a8ebab85f970c7db5', error => {
                 if (error) {
                     console.error(error)
 
                     return
                 }
 
-                console.log('deleted parking')
+                console.log('deleted parking review')
             })
         } catch (error) {
             console.error(error)
