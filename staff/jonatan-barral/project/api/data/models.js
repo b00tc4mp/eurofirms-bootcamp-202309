a4bf93a98fe322f2ac44f81e764339495267d2bb
Jonatan Barral <jinetefugaz@gmail.com>
2024-01-02 16:47:08 +0100

    update data model code, create competition, horse, test, reprise, excercise and valuation model

--- staff/jonatan-barral/project/api/data/models.js
+++ staff/jonatan-barral/project/api/data/models.js
@@ -34,7 +34,7 @@ const user = new Schema({
 
 })
 
-const event = new Schema({
+const competition = new Schema({
     date: {
         type: String,
         required: true
@@ -75,7 +75,9 @@ const competitor = new Schema({
     license: {
         type: String,
     },
+})
 
+const horse = new Schema({
     horseName: {
         type: String,
         required: true
@@ -83,6 +85,14 @@ const competitor = new Schema({
 
     horseLicense: {
         type: String,
+    }
+})
+
+const test = new Schema({
+    competition: {
+        type: ObjectId,
+        ref: 'competition',
+        required: true
     },
 
     trialType: {
@@ -91,50 +101,59 @@ const competitor = new Schema({
         required: true
     },
 
-    dressageLevel: {
+    level: {
         type: String,
-        enum: ['0', '1', '2', '3', '4', 'San Jorge', 'Intermedia 1', 'Intermedia 2', 'intermedia A-B', 'Gran Premio']
-
+        enum: ['0', '1', '2', '3', '4', 'San Jorge', 'Intermedia 1', 'Intermedia 2', 'intermedia A-B', 'Gran Premio', 'Doma Paralímpica']
     },
 
-    dressageRepriseType: {
+    reprise: {
+        type: ObjectId,
+        ref: 'reprise',
+        required: true
+    }
+
+})
+
+const reprise = new Schema({
+    type: {
         type: String,
-        enum: ['AP Final', 'AP Preliminar', 'Benjamines 1', 'Benjamines 2', '4 años experimental', '4 años preliminar', '4 años final', 'Alevines equipos', 'Alevines individual', 'Alevines preliminar', 'Promoción 1', 'Rider 1A', 'Rider 1B', 'Rider 1C', '5 años final', '5 años preliminar', 'Infantiles Equipos', 'Infantiles Individual', 'Infantiles Preliminar A', 'Infantiles Preliminar B', 'Promoción 2', 'Rider 2A', 'Rider 2B', 'Rider 2C', 'Juveniles 0 Equipos', 'Juveniles 0 Individual', 'Juveniles 0 Preliminar', 'Kur Juveniles 0', 'Promoción 3', 'Rider 3A', 'rider 3B', 'Rider 3C', 'Rider 3D', 'Ponis Equipos', 'Ponis Individual', 'Kur Ponis', 'Ponis Preliminar', '6 Años Final', '6 Años Preliminar', 'Clásica 1', 'Clásica 2', 'Clásica 3', 'Juveniles Individual', 'Juveniles Preliminar', 'Juveniles Equipos', 'Kur Juveniles', 'Promoción 4', 'Jóvenes Jinetes Equipos', 'Jóvenes Jinetes Individual', 'Jóvenes Jinetes Preliminar', 'Kur Jóvenes Jinetes', 'Preliminar Caballos 7 Años', 'Final Caballos Jóvenes', 'San Jorge', 'Intermedia I', 'Kur Intermedia I', 'Intermedia II', 'Intermedia A', 'Intermedia B', 'Kur Intermedia A-B', 'Gran Premio Caballos Jóvenes J 8', 'Gran Premio Especial', 'Gran Premio U25', 'Gran Premio', 'Kur Gran Premio']
+        enum: ['AP Final', 'AP Preliminar', 'Benjamines 1', 'Benjamines 2', '4 años experimental', '4 años preliminar', '4 años final', 'Alevines equipos', 'Alevines individual', 'Alevines preliminar', 'Promoción 1', 'Rider 1A', 'Rider 1B', 'Rider 1C', '5 años final', '5 años preliminar', 'Infantiles Equipos', 'Infantiles Individual', 'Infantiles Preliminar A', 'Infantiles Preliminar B', 'Promoción 2', 'Rider 2A', 'Rider 2B', 'Rider 2C', 'Juveniles 0 Equipos', 'Juveniles 0 Individual', 'Juveniles 0 Preliminar', 'Kur Juveniles 0', 'Promoción 3', 'Rider 3A', 'rider 3B', 'Rider 3C', 'Rider 3D', 'Ponis Equipos', 'Ponis Individual', 'Kur Ponis', 'Ponis Preliminar', '6 Años Final', '6 Años Preliminar', 'Clásica 1', 'Clásica 2', 'Clásica 3', 'Juveniles Individual', 'Juveniles Preliminar', 'Juveniles Equipos', 'Kur Juveniles', 'Promoción 4', 'Jóvenes Jinetes Equipos', 'Jóvenes Jinetes Individual', 'Jóvenes Jinetes Preliminar', 'Kur Jóvenes Jinetes', 'Preliminar Caballos 7 Años', 'Final Caballos Jóvenes', 'San Jorge', 'Intermedia I', 'Kur Intermedia I', 'Intermedia II', 'Intermedia A', 'Intermedia B', 'Kur Intermedia A-B', 'Gran Premio Caballos Jóvenes J 8', 'Gran Premio Especial', 'Gran Premio U25', 'Gran Premio', 'Kur Gran Premio', 'Grado I Novel A', 'Grado I Novel B', 'Grado I Intermedia A', 'Grado I Intermedia B', 'Grado I Gran Premio A', 'Grado I Gran Premio B', 'Grado I Kür Gran Premio', 'Grado II Novel A', 'Grado II Novel B', 'Grado II Intermedia A', 'Grado II Intermedia B', 'Grado II Gran Premio A', 'Grado II Gran Premio B', 'Grado II Kür Gran Premio', 'Grado III Novel A', 'Grado III Novel B', 'Grado III Intermedia A', 'Grado III Intermedia B', 'Grado III Gran Premio A', 'Grado III Gran Premio B', 'Grado III Kür Gran Premio', 'Grado IV Novel A', '0Grado IV Novel B', 'Grado IV Intermedia A', 'Grado IV Intermedia B', 'Grado IV Gran Premio A', 'Grado IV Gran Premio B', 'Grado IV Kür Gran Premio', 'Grado V Novel A', 'Grado V Novel B', 'Grado V Intermedia A', 'Grado V Intermedia B', 'Grado V Gran Premio A', 'Grado V Gran Premio B', 'Grado V Kür Gran Premio', 'Grado VI-A Novel A', 'Grado VI-A novel B', 'Grado VI-A Intermedia A', 'Grado VI-A Intermedia B', 'Grado VI-A Gran Premio A', 'Grado VI-A Gran Premio B', 'Grado VI-A Kür Gran Premio', 'Grado VI-B Novel A', 'Grado VI-B novel B', 'Grado VI-B Intermedia A', 'Grado VI-B Intermedia B', 'Grado VI-B Gran Premio A', 'Grado VI-B Gran Premio B', 'Grado VI-B Kür Gran Premio'],
+        required: true
     },
 
-    paralimpicGrade: {
-        type: String,
-        enum: ['grado I', 'Grado II', 'Grado III', 'Grado IV', 'Grado V', 'Grado VI-A', 'Grado VI-B']
+    exercise: {
+        type: ObjectId,
+        ref: 'exercise',
+        required: true
     },
-
-    paralympicRepriseType: {
-        type: String,
-        enum: ['Grado I Novel A', 'Grado I Novel B', 'Grado I Intermedia A', 'Grado I Intermedia B', 'Grado I Gran Premio A', 'Grado I Gran Premio B', 'Grado I Kür Gran Premio', 'Grado II Novel A', 'Grado II Novel B', 'Grado II Intermedia A', 'Grado II Intermedia B', 'Grado II Gran Premio A', 'Grado II Gran Premio B', 'Grado II Kür Gran Premio', 'Grado III Novel A', 'Grado III Novel B', 'Grado III Intermedia A', 'Grado III Intermedia B', 'Grado III Gran Premio A', 'Grado III Gran Premio B', 'Grado III Kür Gran Premio', 'Grado IV Novel A', '0Grado IV Novel B', 'Grado IV Intermedia A', 'Grado IV Intermedia B', 'Grado IV Gran Premio A', 'Grado IV Gran Premio B', 'Grado IV Kür Gran Premio', 'Grado V Novel A', 'Grado V Novel B', 'Grado V Intermedia A', 'Grado V Intermedia B', 'Grado V Gran Premio A', 'Grado V Gran Premio B', 'Grado V Kür Gran Premio', 'Grado VI-A Novel A', 'Grado VI-A novel B', 'Grado VI-A Intermedia A', 'Grado VI-A Intermedia B', 'Grado VI-A Gran Premio A', 'Grado VI-A Gran Premio B', 'Grado VI-A Kür Gran Premio', 'Grado VI-B Novel A', 'Grado VI-B novel B', 'Grado VI-B Intermedia A', 'Grado VI-B Intermedia B', 'Grado VI-B Gran Premio A', 'Grado VI-B Gran Premio B', 'Grado VI-B Kür Gran Premio']
-    },
-
-
 
 })
 
 const exercise = new Schema({
-    mark: {
+    order: {
         type: number,
+    },
+
+    letters: {
+        type: String,
+        required: true
+    },
+
+    movementsw: {
+        type: String,
         required: true
     },
 
     coefficient: {
         type: number,
         enum: [1, 2, 3, 4],
-        default: 1
+        default: 1,
+        required: true
     },
 
-    finalMark: {
-        type: number
-
-    },
-
-    coment: {
-        type: String
+    directives: {
+        type: String,
+        required: true
     }
 
 
@@ -142,31 +161,68 @@ const exercise = new Schema({
 
 
 const valuation = new Schema({
+    competition: {
+        type: ObjectId,
+        ref: 'competition',
+        required: true
+    },
+
     judge: {
         type: ObjectId,
-        ref: 'User'
+        ref: 'User',
+        required: true
+
     },
 
-    Competitor: {
+    competitor: {
         type: ObjectId,
-        Ref: 'Competitor'
+        Ref: 'Competitor',
+        required: true
+
     },
 
-    exercise: [exercise],
+    horse: {
+        type: ObjectId,
+        ref: 'horse',
+        required: true
 
-    finalScore: {
-        Type: sum
-    }
+    },
+
+    exercise: {
+        type: ObjectId,
+        ref: 'exercise',
+        required: true
+
+    },
+
+
 })
 
 const User = model('User', user)
 
-const Event = model('Event', event)
+const Competition = model('Comptetition', competition)
 
 const Competitor = model('Competitor', competitor)
 
+const Horse = model('Horse', horse)
+
+const Test = model('Test', test)
+
+const Reprise = model('Reprise', reprise)
+
+const Exercise = model('Exercise', exercise)
+
+const Valuation = model('Valuation', valuation)
+
+
 module.exports = {
     User,
-    Event,
-    Competitor
+    Competition,
+    Competitor,
+    Horse,
+    Test,
+    Reprise,
+    Exercise,
+    Valuation
+
 }
--- staff/jonatan-barral/project/doc/README.md
+++ staff/jonatan-barral/project/doc/README.md
@@ -73,7 +73,7 @@ La propia app debería devolver un pdf con el formato de repris que se entregar�
 - competitors ([Competitor.id])
 - location (string, required)
         
-#### Competitor 
+#### Competitor
 
 - id (string, unique, required)
 - fullName (string, required)
@@ -89,25 +89,32 @@ La propia app debería devolver un pdf con el formato de repris que se entregar�
 
 - id (string, unique, required)
 - competition (Competition.id)
-- trialType (string, official|open, required)
-- discipline (string, dressage|paralympic )
-- dressagelevel (string, 0|1|2|3|4|San Jorge|Intermedia 1|Intermedia 2|Intermedia AB|Gran Premio)
-- dressageRepriseType (string, AP Final|AP Preliminar|Benjamines 1|Benjamines 2|4 años experimental|4 años preliminar|4 años final|Alevines equipos|Alevines individual|Alevines preliminar|Promoción 1|Rider 1A|Rider 1B|Rider 1C|5 años final|5 años preliminar|Infantiles Equipos|Infantiles Individual|Infantiles Preliminar A|Infantiles Preliminar B|Promoción 2|Rider 2A|Rider 2B|Rider 2C|Juveniles 0 Equipos|Juveniles 0 Individual|Juveniles 0 Preliminar|Kur Juveniles 0|Promoción 3|Rider 3A|rider 3B|Rider 3C|Rider 3D|Ponis Equipos|Ponis Individual|Kur Ponis|Ponis Preliminar|6 Años Final|6 Años Preliminar|Clásica 1|Clásica 2|Clásica 3|Juveniles Individual|Juveniles Preliminar|Juveniles Equipos|Kur Juveniles|Promoción 4|Jóvenes Jinetes Equipos|Jóvenes Jinetes Individual|Jóvenes Jinetes Preliminar|Kur Jóvenes Jinetes|Preliminar Caballos 7 Años|Final Caballos Jóvenes|San Jorge|Intermedia I|Kur Intermedia I|Intermedia II|Intermedia A|Intermedia B|Kur Intermedia A-B|Gran Premio Caballos Jóvenes J 8|Gran Premio Especial|Gran Premio U25|Gran Premio|Kur Gran Premio')
-- paralympicGrade (string, Grado I|Grado II|Grado III|Grado IV|Grado V|Grado VI-A|Grado VI-B)
-- paralympicRepriseType (string, ['Grado I Novel A|Grado I Novel B|Grado I Intermedia A|Grado I Intermedia B|Grado I Gran Premio A|Grado I Gran Premio B|Grado I Kür Gran Premio|Grado II Novel A|Grado II Novel B|Grado II Intermedia A|Grado II Intermedia B|Grado II Gran Premio A|Grado II Gran Premio B|Grado II Kür Gran Premio|Grado III Novel A|Grado III Novel B|Grado III Intermedia A|Grado III Intermedia B|Grado III Gran Premio A|Grado III Gran Premio B|Grado III Kür Gran Premio|Grado IV Novel A|0Grado IV Novel B|Grado IV Intermedia A|Grado IV Intermedia B|Grado IV Gran Premio A|Grado IV Gran Premio B|Grado IV Kür Gran Premio|Grado V Novel A|Grado V Novel B|Grado V Intermedia A|Grado V Intermedia B|Grado V Gran Premio A|Grado V Gran Premio B|Grado V Kür Gran Premio|Grado VI-A Novel A|Grado VI-A novel B|Grado VI-A Intermedia A|Grado VI-A Intermedia B|Grado VI-A Gran Premio A|Grado VI-A Gran Premio B|Grado VI-A Kür Gran Premio|Grado VI-B Novel A|Grado VI-B novel B|Grado VI-B Intermedia A|Grado VI-B Intermedia B|Grado VI-B Gran Premio A|Grado VI-B Gran Premio B|Grado VI-B Kür Gran Premio')
+- trial (string, official|open, required)
+- level (string, Nivel 0 | Nivel 1 | Nivel 2 | Nivel 3 | Nivel 4 | San Jorge | Intermedia 1 | Intermedia 2 | Intermedia A-B | Gran Premio | Paralimpica)
+- reprise (Reprise.id)
 
-#### Exercises
+#### Reprise
 
-- mark (number, required)
-- coefficient (number 1|2|3|4, )
-- finalMark (multiplication, mark * coefficient )
-- coment (string)
+- id (string, required, unique)
+- type (string, AP Final|AP Preliminar|Benjamines 1|Benjamines 2|4 años experimental|4 años preliminar|4 años final|Alevines equipos|Alevines individual|Alevines preliminar|Promoción 1|Rider 1A|Rider 1B|Rider 1C|5 años final|5 años preliminar|Infantiles Equipos|Infantiles Individual|Infantiles Preliminar A|Infantiles Preliminar B|Promoción 2|Rider 2A|Rider 2B|Rider 2C|Juveniles 0 Equipos|Juveniles 0 Individual|Juveniles 0 Preliminar|Kur Juveniles 0|Promoción 3|Rider 3A|rider 3B|Rider 3C|Rider 3D|Ponis Equipos|Ponis Individual|Kur Ponis|Ponis Preliminar|6 Años Final|6 Años Preliminar|Clásica 1|Clásica 2|Clásica 3|Juveniles Individual|Juveniles Preliminar|Juveniles Equipos|Kur Juveniles|Promoción 4|Jóvenes Jinetes Equipos|Jóvenes Jinetes Individual|Jóvenes Jinetes Preliminar|Kur Jóvenes Jinetes|Preliminar Caballos 7 Años|Final Caballos Jóvenes|San Jorge|Intermedia I|Kur Intermedia I|Intermedia II|Intermedia A|Intermedia B|Kur Intermedia A-B|Gran Premio Caballos Jóvenes J 8|Gran Premio Especial|Gran Premio U25|Gran Premio|Kur Gran Premio | Grado I Novel A|Grado I Novel B|Grado I Intermedia A|Grado I Intermedia B|Grado I Gran Premio A|Grado I Gran Premio B|Grado I Kür Gran Premio|Grado II Novel A|Grado II Novel B|Grado II Intermedia A|Grado II Intermedia B|Grado II Gran Premio A|Grado II Gran Premio B|Grado II Kür Gran Premio|Grado III Novel A|Grado III Novel B|Grado III Intermedia A|Grado III Intermedia B|Grado III Gran Premio A|Grado III Gran Premio B|Grado III Kür Gran Premio|Grado IV Novel A|0Grado IV Novel B|Grado IV Intermedia A|Grado IV Intermedia B|Grado IV Gran Premio A|Grado IV Gran Premio B|Grado IV Kür Gran Premio|Grado V Novel A|Grado V Novel B|Grado V Intermedia A|Grado V Intermedia B|Grado V Gran Premio A|Grado V Gran Premio B|Grado V Kür Gran Premio|Grado VI-A Novel A|Grado VI-A novel B|Grado VI-A Intermedia A|Grado VI-A Intermedia B|Grado VI-A Gran Premio A|Grado VI-A Gran Premio B|Grado VI-A Kür Gran Premio|Grado VI-B Novel A|Grado VI-B novel B|Grado VI-B Intermedia A|Grado VI-B Intermedia B|Grado VI-B Gran Premio A|Grado VI-B Gran Premio B|Grado VI-B Kür Gran Premio)
+- exercises ([Exercise.id, required])
+
+#### Exercise
+
+- id (string, unique, required)
+- order (number, 1-N)
+- letters (string, required)
+- movements (string, required)
+- coefficient (number 1|2|3|4, required)
+- directives (string, required)
 
 #### Valuation
 
 - id (string, unique, required)
+- competition (Competition.id, rquired)
 - judge (User.id, required)
-- competitor (Competitor.id)
-- exercises ([exercises])
-- finalScore (sum finalMark )
-- percentageScore (finalScore * 100 / total)
+- competitor (Competitor.id, required)
+- horse (Horse.id, required)
+- exercise (Exercise.id)
+- mark (number, required)
+- comment (string)
