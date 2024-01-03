c40c6e04de15fc29c0b4e774abcdd6544b3c1cc1
Johonas Sanchez <johonas.s.nava@gmail.com>
2024-01-03 11:47:32 +0100

    update retrieveUser logic #97

--- staff/johonas-sanchez/project/api/logic/retrieveUser.js
+++ staff/johonas-sanchez/project/api/logic/retrieveUser.js
@@ -2,19 +2,24 @@ const { validate } = require("./helpers")
 const { User } = require("../data/models")
 const { NotFoundError, SystemError } = require("./errors")
 
-function retrieveUser(userId) {
+function retrieveUser(userId, callback) {
    validate.id(userId, "user id")
+   validate.function(callback, "callback")
 
-   return User.findById(userId).select("-password -__v").lean()
-         // User.findById(userId).lean() Con el select en negativo puedo eliminar las propiedades que me devuelve
+   User.findById(userId)
+      .select("-password -__v")
+      .lean()
+      // User.findById(userId).lean() Con el select en negativo puedo eliminar las propiedades que me devuelve
 
-         .catch(error => { throw new SystemError(error.message) })
-         .then(user => {
-            if (!user) 
-               throw new NotFoundError("user not found")
+      .then((user) => {
+         if (!user) {
+            callback(new NotFoundError("user not found"))
 
-            return user
-         })
+            return
+         }
+         callback(null, user)
+      })
+      .catch((error) => callback(new SystemError(error.message)))
 }
 
 module.exports = retrieveUser
--- staff/johonas-sanchez/project/api/logic/retrieveUser.test.js
+++ staff/johonas-sanchez/project/api/logic/retrieveUser.test.js
@@ -5,10 +5,16 @@ const retrieveUser = require('./retrieveUser')
 mongoose.connect('mongodb://127.0.0.1:27017/projectTest')
     .then(() => {
         try {
-            retrieveUser('6593d3059ea31c753328c2fd')
-                .then(user => console.log('user retrieved', user))
-                .catch(error => console.error(error))
-        } catch (error) {
-            console.error(error)
-        }
-    })
+            retrieveUser("6593d3059ea31c753328c2fd", (error, user) => {
+                if (error) {
+                   console.error(error)
+    
+                   return
+                }
+                console.log(user)
+             })
+          } catch (error) {
+             console.error(error)
+          }
+       })
+    
