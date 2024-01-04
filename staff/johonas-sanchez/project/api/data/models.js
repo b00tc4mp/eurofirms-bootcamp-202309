493d7779c72843c11ee127dc0afa35775d0e6050
Johonas Sanchez <johonas.s.nava@gmail.com>
2024-01-04 14:04:16 +0100

    update quotes style #97

--- staff/johonas-sanchez/project/api/data/models.js
+++ staff/johonas-sanchez/project/api/data/models.js
@@ -24,76 +24,72 @@ const user = new Schema({
    },
    role: {
       type: String,
-      enum: ['Manager', 'User'],
-      required: true
-  },
+      enum: ["Manager", "User"],
+      required: true,
+   },
    saved: {
       type: [ObjectId],
-      ref: 'Parking',
+      ref: "Parking",
    },
 })
 
 const point = new Schema({
    type: {
       type: String,
-      default: 'Point',
-      required: true
-    },
-    coordinates: {
+      default: "Point",
+      required: true,
+   },
+   coordinates: {
       type: [Number],
-      required: true
-    }
+      required: true,
+   },
 })
 
 const parking = new Schema({
    location: {
       type: point,
       required: true,
-      index: '2dsphere'
+      index: "2dsphere",
    },
    locator: {
       type: ObjectId,
       required: true,
-      ref: "User"
+      ref: "User",
    },
    confirmations: {
       type: [ObjectId],
-      ref: 'User'
+      ref: "User",
    },
-
 })
 
 const review = new Schema({
    parking: {
       type: ObjectId,
-      ref: 'Parking',
+      ref: "Parking",
       required: true,
    },
-    author: {
-       type: ObjectId,
-       required: true,
-       ref: 'User',
-    },
-    comment: {
-        type: String,
-     },
-     valuation: {
-        type: Number,
-        required: true,
-     },
-
- })
-
+   author: {
+      type: ObjectId,
+      required: true,
+      ref: "User",
+   },
+   comment: {
+      type: String,
+   },
+   valuation: {
+      type: Number,
+      required: true,
+   },
+})
 
 const User = model("User", user)
 const Point = model("Point", point)
 const Parking = model("Parking", parking)
 const Review = model("Review", review)
 
-
 module.exports = {
    User,
    Point,
    Parking,
-   Review
+   Review,
 }
