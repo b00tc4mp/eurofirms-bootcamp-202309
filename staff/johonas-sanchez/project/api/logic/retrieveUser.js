94b3e91d7c826d0a0873bb12cdb5874c24676d87
Johonas Sanchez <johonas.s.nava@gmail.com>
2024-01-08 10:12:00 +0100

    sanitize retrieveUser #106

--- staff/johonas-sanchez/project/api/logic/retrieveUser.js
+++ staff/johonas-sanchez/project/api/logic/retrieveUser.js
@@ -5,18 +5,24 @@ const { NotFoundError, SystemError } = require("./errors")
 function retrieveUser(userId) {
    validate.id(userId, "user id")
 
-   return User.findById(userId)
-      .select("-password -__v")
-      .lean()
-      // User.findById(userId).lean() Con el select en negativo puedo eliminar las propiedades que me devuelve
+   return (
+      User.findById(userId)
+         .select("-password -__v")
+         .lean()
+         // User.findById(userId).lean() Con el select en negativo puedo eliminar las propiedades que me devuelve
 
-      .then(user => {
-         if (!user) 
-            throw new NotFoundError("user not found")
+         .then(user => {
+            if (!user) throw new NotFoundError("user not found")
 
-         return user
-      })
-      .catch(error => { throw new SystemError(error.message) })
+            user.id = user._id.toString()
+            delete user._id
+
+            return user
+         })
+         .catch((error) => {
+            throw new SystemError(error.message)
+         })
+   )
 }
 
 module.exports = retrieveUser
