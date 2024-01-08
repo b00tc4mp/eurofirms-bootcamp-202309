edf73957de2bdb91e1f046cc6fec4861412f1ce1
David Cano <webdelcano@gmail.com>
2024-01-08 18:13:57 +0100

    add copy_end_fullstack folder

---
+++ staff/david-cano/fullstack/pruebas/David_FullStack/copy_end_fullstack/api/test/retrievePosts.test.js
@@ -0,0 +1,22 @@
+const req = {
+    method: 'GET',
+    headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTc1MjNhMmM1YmZkMDU3ZWY1NWE5NzEiLCJpYXQiOjE3MDI1MTg3Mjd9.x_cj7ACxNeJGZPOIycSy6CI87TgXXnV16uq7i8s2s7Y',
+    },
+}
+
+fetch('http://localhost:4000/posts', req)
+    .then(res => {
+        if (!res.ok) {
+            res.json()
+                .then(body => console.error(body))
+                .catch(error => console.error(error))
+
+            return
+        }
+
+        res.json()
+            .then(posts => console.log(res.status, posts))
+            .catch(error => console.error(error))
+    })
+    .catch(error => console.error(error))
---
+++ staff/david-cano/fullstack/pruebas/David_FullStack/copy_end_fullstack/app/src/App.jsx
@@ -0,0 +1,67 @@
+import { useState } from 'react'
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
+
+import Feedback from './library/Feedback'
+
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+
+import Hello from './components/Hello'
+
+import logic from './logic'
+import { CredentialsError, JWTError, SystemError } from './logic/errors'
+
+export default function App() {
+    console.log('App')
+
+    const [feedback, setFeedback] = useState(null)
+    const navigate = useNavigate()
+
+    function handleShowRegister() {
+        navigate('/register')
+        setFeedback(null)
+    }
+
+    function handleShowLogin() {
+        navigate('/login')
+        setFeedback(null)
+    }
+
+    function handleShowHome() {
+        navigate('/')
+        setFeedback(null)
+    }
+
+    function handleError(error) {
+        if (error instanceof JWTError) {
+            logic.logoutUser()
+
+            navigate('/login')
+            setFeedback('SESSION IS EXPIRED!!!; please "LOGIN" again')
+        } else if (error instanceof CredentialsError) {
+            setFeedback('Wrong credentials, try again')
+        } else if (error instanceof SystemError) {
+            setFeedback('Something went wrong. Please, try again later')
+        }
+        else setFeedback(error.message)
+    }
+
+    function handleAcceptFeedback() {
+        setFeedback(null)
+    }
+
+    return <>
+        <Routes>
+            <Route path="/login" element={logic.isUserLoggedIn() ? <Navigate to="/" /> : <Login onSuccess={handleShowHome} onRegisterClick={handleShowRegister} onError={handleError} />} />
+
+            <Route path="/register" element={logic.isUserLoggedIn() ? <Navigate to="/" /> : <Register onSuccess={handleShowLogin} onLoginClick={handleShowLogin} onError={handleError} />} />
+
+            <Route path="/*" element={logic.isUserLoggedIn() ? <Home onLogout={handleShowLogin} onError={handleError} /> : <Navigate to="/login" />} />
+
+            <Route path="/hello/:name" element={<Hello />} />
+        </Routes>
+
+        {feedback ? <Feedback message={feedback} onAccept={handleAcceptFeedback} /> : null}
+    </>
+}
