a571cad4983d29d95735af84de155d845a3ae4db
Johonas Sanchez <johonas.s.nava@gmail.com>
2024-01-09 15:39:59 +0100

    add and run create-parking.test.sh #106

---
+++ staff/johonas-sanchez/project/api/test/create-parking.test.sh
@@ -0,0 +1,1 @@
+curl -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NTk2ZDE1Mzc5N2ZmYTg0MWI0MTNlODYiLCJpYXQiOjE3MDQ4MDA2NzV9.vY74DpSJqCCa5JrujpzwWt1DRiHGkIqMs2lscy__yvU' -H 'Content-Type: application/json' -d '{ "lat": 40.029520, "long": -6.085075 }' localhost:4000/parkings -v
