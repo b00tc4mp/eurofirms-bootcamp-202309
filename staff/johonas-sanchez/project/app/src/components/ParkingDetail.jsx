89a1cad333f7a1e8ea4a12149c4bf3aebb9312ed
Johonas Sanchez <johonas.s.nava@gmail.com>
2024-01-21 09:00:00 +0100

    show details of retrieveParking #110

--- staff/johonas-sanchez/project/app/src/components/ParkingDetail.jsx
+++ staff/johonas-sanchez/project/app/src/components/ParkingDetail.jsx
@@ -9,22 +9,21 @@ import logic from "../logic"
 function ParkingDetail(props) {
    console.log("Parking Detail")
 
-
    const navigate = useNavigate()
    
    const { parkingId } = useParams()
-   // const [parkingData, setParkingData] = useState(null)
+   const [parkingData, setParkingData] = useState(null)
 
-   // useEffect(() => {
-   //    logic
-   //       .retrieveParking(parkingId)
-   //       .then((data) => {
-   //          setParkingData(data)
-   //       })
-   //       .catch((error) => {
-   //          props.onError(error)
-   //       })
-   // }, [parkingId,props])
+   useEffect(() => {
+      logic
+         .retrieveParking(parkingId)
+         .then((data) => {
+            setParkingData(data)
+         })
+         .catch((error) => {
+            props.onError(error)
+         })
+   }, [parkingId,props])
 
    function handleReturnClick() {
       navigate("/")
@@ -35,10 +34,15 @@ function ParkingDetail(props) {
          <Link className="text-blue-700 hover:underline" onClick={handleReturnClick}>
             &lt; Volver
          </Link>
-         <h1 className="mt-4">Parking Detail</h1> 
-            <p>
+         <h1 className="my-4">Parking Detail</h1> 
+         <div className="border-solid border border-black p-3">
+            <p className="mb-3">
                Parking ID: <strong>{parkingId}</strong>
             </p>
+            <p>
+               Parking confirmado por: <strong>{parkingData.confirmations.length} usuarios</strong>
+            </p>
+            </div>
       </div>
    )
 }
