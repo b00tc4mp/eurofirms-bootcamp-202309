484770d85206b289dd8ee18f827dc93535d9af3b
David Cano <webdelcano@gmail.com>
2024-02-02 02:17:53 +0100

    add App components pages #123

---
+++ staff/david-cano/project/app/src/components/index.js
@@ -0,0 +1,9 @@
+import AllProducts from './AllProducts'
+import Header from './Header'
+import Footer from './Footer'
+
+export {
+    AllProducts,
+    Header,
+    Footer
+}
--- staff/david-cano/project/app/src/pages/Home.jsx
+++ staff/david-cano/project/app/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import {AllProducts, Header, Footer} from '../components'
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Header />
+            <AllProducts/>
+            <Footer />
+        </div>
     )
 }
 
